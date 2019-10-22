52aa41ef8714796285cf70ed7803cd24e28b0d27
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2019-10-22 23:33:14 +0100

    remove install-stack

--- packages/haskell/scripts/install-stack.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { }, myLib ? import ../../lib { pkgs = pkgs; } }:
-
-let
-
-  src = builtins.fetchurl "https://get.haskellstack.org/";
-
-in myLib.shellScript "install-stack" ''
-  sh ${src} -d $HOME/.local/bin
-''
