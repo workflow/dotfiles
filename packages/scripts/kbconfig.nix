f7ff89778612b6b8e6b25efb10c720320857bad7
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2019-11-04 22:23:18 +0000

    fix kbconfig

--- packages/scripts/kbconfig.nix
+++ packages/scripts/kbconfig.nix
@@ -3,13 +3,14 @@
 pkgs.myLib.shellScript "kbconfig" ''
   #!${pkgs.bash}/bin/bash
 
+  setxkbmap -option
   setxkbmap -option ctrl:ralt_rctrl
   setxkbmap -layout us,gr -option grp:alt_space_toggle
 
   if [ "$1" == "both" ]; then
     xset r rate 500 45
     setxkbmap -option ctrl:nocaps
-    ${pkgs.xcape}/bin/xcape -t 300 -e 'Control_L=Escape'
+    ${pkgs.xcape}/bin/xcape -t "$${2:-300}" -e 'Control_L=Escape'
   else
     ${pkgs.killall}/bin/killall xcape || true
     xset r rate 500 45
