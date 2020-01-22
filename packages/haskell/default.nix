33f8cf82bc103a17d2d95d93d6bc6951e19bd830
Alex Peitsinis <alex.peitsinis@habito.com>
2020-01-22 10:53:35 +0000

    Add ghc-lib-parser-ex and update hlint

--- packages/haskell/default.nix
+++ packages/haskell/default.nix
@@ -5,14 +5,27 @@ let
   sources = import ../../nix/sources.nix;
   nixos-beta = import sources.nixos-beta { };
 
-  overrides = {
-    ghc-lib-parser = nixos-beta.haskellPackages.ghc-lib-parser_8_8_1;
+  ghc-lib-parser = nixos-beta.haskellPackages.ghc-lib-parser_8_8_1;
+  ghc-lib-parser-ex-pkg = "ghc-lib-parser-ex-8.8.2";
+  ghc-lib-parser-ex-src = fetchTarball {
+    url =
+      "https://hackage.haskell.org/package/${ghc-lib-parser-ex-pkg}/${ghc-lib-parser-ex-pkg}.tar.gz";
+    sha256 = "0ff1rb53wmbkbdral725brs0wg2bg4x2bb2klfwa2cqix1qi68lv";
   };
+  ghc-lib-parser-ex = pkgs.haskellPackages.callCabal2nix "ghc-lib-parser-ex"
+    ghc-lib-parser-ex-src { ghc-lib-parser = ghc-lib-parser; };
 
   fast-tags = pkgs.callPackage ./fast-tags.nix { };
-  hlint = pkgs.callPackage ./hlint.nix { overrides = overrides; };
+  hlint = pkgs.callPackage ./hlint.nix {
+    overrides = {
+      ghc-lib-parser = ghc-lib-parser;
+      ghc-lib-parser-ex = ghc-lib-parser-ex;
+    };
+  };
   ghcid = pkgs.callPackage ./ghcid.nix { };
-  ormolu = pkgs.callPackage ./ormolu.nix { overrides = overrides; };
+  ormolu = pkgs.callPackage ./ormolu.nix {
+    overrides = { ghc-lib-parser = ghc-lib-parser; };
+  };
 
   ghc = pkgs.callPackage ./ghc.nix { };
 
--- packages/haskell/hlint.nix
+++ packages/haskell/hlint.nix
@@ -5,8 +5,8 @@ let
   src = pkgs.fetchFromGitHub {
     owner = "ndmitchell";
     repo = "hlint";
-    rev = "5f267eb873e349fc010a4a0da7778211853b6f03";
-    sha256 = "1dnsf7m8x2ghykgyzpjw0wh53spzcwk4ybg2gmvshvqkvpz9w4zi";
+    rev = "f404c85ef5e14bff6e0216072e04ea040821fbbd";
+    sha256 = "0pyiyvn8hc8i59v58k484ph2yqvaai53w8h4ms0x8q9dw0ljj1hr";
   };
 
 in pkgs.haskellPackages.callCabal2nix "hlint" src overrides
