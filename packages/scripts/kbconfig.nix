253963fa3731200c3b46252f0da92d58096f78c9
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-03-28 13:08:54 +0000

    update kbconfig

--- packages/scripts/kbconfig.nix
+++ packages/scripts/kbconfig.nix
@@ -3,6 +3,9 @@
 let
 
   myLib = pkgs.callPackage ../../lib.nix { };
+  setxkbmap = "${pkgs.xorg.setxkbmap}/bin/setxkbmap";
+  xset = "${pkgs.xorg.xset}/bin/xset";
+  xcape = "${pkgs.xcape}/bin/xcape";
 
 in
 
@@ -10,20 +13,20 @@ myLib.shellScript "kbconfig" ''
   #!${pkgs.bash}/bin/bash
 
   ${pkgs.killall}/bin/killall xcape || true
-  setxkbmap -option
-  setxkbmap -option ctrl:ralt_rctrl
-  setxkbmap -layout us,gr -option grp:alt_space_toggle
+  ${setxkbmap} -option
+  ${setxkbmap} -option ctrl:ralt_rctrl
+  ${setxkbmap} -layout us,gr -option grp:alt_space_toggle
 
   if [ "$1" == "both" ]; then
-    xset r rate 500 45
-    setxkbmap -option ctrl:nocaps
-    ${pkgs.xcape}/bin/xcape -t 300 -e 'Control_L=Escape'
+    ${xset} r rate 500 45
+    ${setxkbmap} -option ctrl:nocaps
+    ${xcape} -t 300 -e 'Control_L=Escape'
   elif [ "$1" == "keep" ]; then
-    xset r rate 500 45
-    setxkbmap -option ctrl:nocaps
-    ${pkgs.xcape}/bin/xcape -d -t 300 -e 'Control_L=Escape'
+    ${xset} r rate 500 45
+    ${setxkbmap} -option ctrl:nocaps
+    ${xcape} -d -t 300 -e 'Control_L=Escape'
   else
-    xset r rate 500 45
-    setxkbmap -option caps:escape
+    ${xset} r rate 500 45
+    ${setxkbmap} -option caps:escape
   fi
 ''
