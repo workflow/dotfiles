00dcd7f56233ab28912702e277d41ce9daf13372
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-04-24 00:34:29 +0100

    smaller tray

--- packages/tools/trayer-wrap.nix
+++ packages/tools/trayer-wrap.nix
@@ -4,7 +4,7 @@ pkgs.writeScriptBin "trayer-wrap" ''
   #!${pkgs.bash}/bin/bash
 
   ${pkgs.trayer}/bin/trayer \
-    --edge top --align right --widthtype percent --width 6 \
+    --edge top --align right --widthtype percent --width 5 \
     --heighttype pixel --height 25 --SetDockType true \
     --SetPartialStrut true --expand false --transparent true \
     --alpha 0 --tint 0x272727
