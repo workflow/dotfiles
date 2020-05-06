520afc126926a72eecd87f43c43299040b965b41
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-05-06 09:31:56 +0100

    home-manager: fix gnome-terminal font formatting

--- home.nix
+++ home.nix
@@ -241,13 +241,20 @@ in
     themeVariant = "dark";
     profile =
       let
-        smallFont = "Source Code Pro Medium-10.5";
-        largeFont = "Source Code Pro Medium-11";
+        fmtFont = { name, size }: "${name} ${builtins.toString size}";
+        smallFont = {
+          name = "Source Code Pro Semibold";
+          size = 10.5;
+        };
+        largeFont = {
+          name = "Source Code Pro Medium";
+          size = 11;
+        };
         dark = {
           default = false;
           visibleName = "Dark";
           showScrollbar = false;
-          font = smallFont;
+          font = fmtFont smallFont;
           colors = {
             foregroundColor = "rgb(161,183,185)";
             backgroundColor = "rgb(24,24,24)";
@@ -276,7 +283,7 @@ in
           default = false;
           visibleName = "Light";
           showScrollbar = false;
-          font = smallFont;
+          font = fmtFont smallFont;
           colors = {
             foregroundColor = "rgb(35,35,35)";
             backgroundColor = "rgb(240,255,240)";
@@ -306,12 +313,12 @@ in
           "a5914944-7bfe-4e88-8699-695bf6ce9f2c" = dark // { default = true; };
           "cd0124dc-173f-430a-a5f0-4eb1847845f4" = dark // {
             visibleName = "Dark large";
-            font = largeFont;
+            font = fmtFont largeFont;
           };
           "71fe2833-7417-43da-8459-008eb2f9e115" = light;
           "636893b8-eb99-4361-a0ff-fe7b5e61e4c7" = light // {
             visibleName = "Light large";
-            font = largeFont;
+            font = fmtFont largeFont;
           };
         };
   };
