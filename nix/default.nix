25bb0495cf81ddb0566bd7a4fc1d737a6172c59a
workflow <florian.peter@gmx.at>
2020-06-24 11:31:52 +0800

    Update nix config

--- nix/default.nix
+++ nix/default.nix
@@ -9,19 +9,11 @@ in
 
   nix = {
     # trusted users for pulling from caches
-    trustedUsers = [ "root" "alex" "@wheel" "@sudo" ];
-    # required for building with bazel
-    nrBuildUsers = 128;
+    trustedUsers = [ "root" "farlion" "@wheel" "@sudo" ];
 
     binaryCaches = [
       "https://cache.nixos.org/"
-      "https://alexpeits.cachix.org"
-      "https://alexpeits-travis.cachix.org"
-    ];
-
-    binaryCachePublicKeys = [
-      "alexpeits.cachix.org-1:O5CoFuKPb8twVOp1OrfSOPfgaEo5X5xlIqGg6dMEgB4="
-      "alexpeits-travis.cachix.org-1:V3Rz9GshL7QTfajKGoUpW8PwqQZSdWmjTK2f/VB1/do="
+      # Add cachix here
     ];
 
     nixPath = [
