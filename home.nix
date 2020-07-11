79b25e4ab893716f98ca85f2dca66203c1d9cc4f
workflow <florian.peter@gmx.at>
2020-07-11 15:11:56 +0800

    Add .stignore for the code share

--- home.nix
+++ home.nix
@@ -99,13 +99,19 @@ in
   };
 
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "20.03";
+  home = {
+    file = {
+      "code/.stignore".source = ./dotfiles/stignore_code;
+    };
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "20.03";
+  };
 }
