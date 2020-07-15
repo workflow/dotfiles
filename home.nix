6ab362debf8540f469ef156ef1b76b4becf2c4f2
workflow <florian.peter@gmx.at>
2020-07-15 17:59:14 +0800

    Enable urxvt extensions and URL matcher

--- home.nix
+++ home.nix
@@ -148,13 +148,16 @@ in
     urxvt = {
       enable = true;
       extraConfig = {
-        #letterSpace = -1;
+        perl-ext-common = "default,matcher";
+        url-launcher = "${pkgs.xdg_utils}/bin/xdg-open";
+        "matcher.button" = 1;
       };
       fonts = [ "xft:Inconsolata:size=9" ];
       keybindings = { 
         "Shift-Control-C" = "eval:selection_to_clipboard";
         "Shift-Control-V" = "eval:paste_clipboard";
       };
+      package = pkgs.rxvt_unicode-with-plugins;
       scroll.bar.enable = false;
     };
   };
