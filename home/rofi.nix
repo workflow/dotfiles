f20f5c5bc90253c87b554d6e736b4fe2c30b5595
workflow <florian.peter@gmx.at>
2020-07-16 17:33:43 +0800

    Style rofi

--- home/rofi.nix
+++ home/rofi.nix
@@ -1,13 +1,53 @@
 { config, lib, pkgs, ... }:
 
+let
+
+  # Colors from: https://github.com/Kthulu120/i3wm-themes/blob/master/Nature/.resources/.extend.Xresources
+  color1 = "#162025";
+  color2 = "#662b37";
+  color3 = "#bfbfbf";
+
+in
+
 {
   programs.rofi = {
     enable = true;
+
     extraConfig = ''
-      rofi.opacity: 85
+      rofi.bw: 1
+      rofi.columns: 2
     '';
+
+    colors = {
+      rows = rec {
+        normal = {
+          background = color1;
+          foreground = color3;
+          backgroundAlt = color1;
+          highlight = {
+            background = color1;
+            foreground = color2;
+          };
+        };
+        active = normal;
+        urgent = normal;
+      };
+
+      window = {
+        background = color1;
+        border = color2;
+        separator = color2;
+      };
+    };
+
     font = "Inconsolata 12";
-    fullscreen = true;
-    theme = "Pop-Dark";
+
+    lines = 5;
+
+    padding = 5;
+
+    scrollbar = false;
+
+    separator = "solid";
   };
 }
