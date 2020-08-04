04b7bce8ae05a2d29db58f293dc3ec77d68efbf8
workflow <florian.peter@gmx.at>
2020-08-04 11:01:17 +0800

    Add more keyboard layouts and configure switching

--- system/keyboard.nix
+++ system/keyboard.nix
@@ -2,7 +2,7 @@
 
 {
   services.xserver = {
-    layout = "us";
-    xkbOptions = "eurosign:e,caps:escape_shifted_capslock,terminate:ctrl_alt_bksp";
+    layout = "us,de,ua";
+    xkbOptions = "grp:alts_toggle,eurosign:e,caps:escape_shifted_capslock,terminate:ctrl_alt_bksp";
   };
 }
