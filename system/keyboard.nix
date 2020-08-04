52f069b0f1e42dad71aff8b5b080313cecf402b0
workflow <florian.peter@gmx.at>
2020-08-05 07:32:27 +0800

    Annotate nixos module with its effect

--- system/keyboard.nix
+++ system/keyboard.nix
@@ -1,6 +1,7 @@
 { ... }:
 
 {
+  # writes to /etc/X11/xorg.conf.d
   services.xserver = {
     layout = "us,de,ua";
     xkbOptions = "grp:alts_toggle,eurosign:e,caps:escape_shifted_capslock,terminate:ctrl_alt_bksp";
