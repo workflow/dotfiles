f83748ae903c3d1828ec14b7f0bb8706e8350270
workflow <florian.peter@gmx.at>
2020-08-25 11:25:58 +0800

    Up default sample rate

--- system/default.nix
+++ system/default.nix
@@ -39,6 +39,11 @@
   hardware.pulseaudio = {
     enable = true;
 
+    # Writes to /etc/pulse/daemon.conf
+    daemon.config = {
+      default-sample-rate = 48000;
+    };
+
     # NixOS allows either a lightweight build (default) or full build of PulseAudio to be installed.
     # Only the full build has Bluetooth support, so it must be selected here.
     package = pkgs.pulseaudioFull;
