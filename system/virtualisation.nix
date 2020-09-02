b1c01482b59f4a944ab713cc10de794e94ec4746
workflow <florian.peter@gmx.at>
2020-09-02 15:56:48 +0800

    Add KVM virtualization

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -3,8 +3,10 @@
 
   virtualisation.docker.enable = true;
 
+  virtualisation.libvirtd.enable = true;
+
   # TODO: This is insecure
   # See https://nixos.wiki/wiki/Docker
-  users.users.farlion.extraGroups = [ "docker" ];
+  users.users.farlion.extraGroups = [ "docker" "libvirtd" ];
 
 }
