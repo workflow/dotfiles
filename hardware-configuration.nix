a6a2e41093ea993558ebfcd73f7102cf25ad83a7
workflow <florian.peter@gmx.at>
2020-09-03 11:21:37 +0800

    Override CPU throttling on T480 only

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,6 +1,7 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
   # For better T480 CPU throttling, see https://github.com/erpalma/throttled
-  services.throttled.enable = true;
+  services.throttled.enable = lib.mkIf (config.networking.hostName == "nixbox") true;
+
 }
