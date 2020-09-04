79705d25727e6464ac1df16fd737def28f29b902
workflow <florian.peter@gmx.at>
2020-09-04 11:41:27 +0800

    Switch to a swapfile for hibernation

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -4,4 +4,11 @@
   # For better T480 CPU throttling, see https://github.com/erpalma/throttled
   services.throttled.enable = lib.mkIf (config.networking.hostName == "nixbox") true;
 
+  swapDevices = [
+    {
+      device = "/var/swap";
+      size = 1024 * 32 * 2; # twice the RAM should leave enough space for hibernation
+    }
+  ];
+
 }
