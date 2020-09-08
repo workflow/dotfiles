56a3f47b8e1f10ad130ca507a44ee4bc673910d2
workflow <florian.peter@gmx.at>
2020-09-08 11:38:06 +0800

    Set logind lidSwitch to lock

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,6 +1,9 @@
 { config, lib, ... }:
 
 {
+
+  services.logind.lidSwitch = "lock";
+
   # For better T480 CPU throttling, see https://github.com/erpalma/throttled
   services.throttled.enable = lib.mkIf (config.networking.hostName == "nixbox") true;
 
