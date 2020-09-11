9b688844f8f6eae735590a09399b319769465b70
workflow <florian.peter@gmx.at>
2020-09-11 17:28:21 +0800

    Add private cachix cache

--- nix/default.nix
+++ nix/default.nix
@@ -13,11 +13,18 @@ in
     binaryCaches = [
       "https://cache.nixos.org/"
       "https://workflow.cachix.org/"
+      "https://hyperion.cachix.org/"
     ];
 
     binaryCachePublicKeys = [
       "workflow.cachix.org-1:HhfBXgXCafJxYuATcMDQbC1qsbjF9qJUCchzFZS2zL4="
+      "hyperion.cachix.org-1:uVV2qr8RIPYHijXSUA/2D24dbLGFDxkhDy4uYQ/LKuw="
     ];
+
+    extraOptions = ''
+      # For private cachix caches
+      netrc-file = /home/farlion/.config/nix/netrc
+    '';
 
     nixPath = [
       "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
