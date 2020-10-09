a34273ea48c9e1933d0fe0458eb502b076374cc5
workflow <florian.peter@gmx.at>
2020-10-09 13:08:44 +0800

    Get rid of swapfile hack on nixbox

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,15 +8,6 @@
   # For better T480 CPU throttling, see https://github.com/erpalma/throttled
   services.throttled.enable = lib.mkIf (config.networking.hostName == "nixbox") true;
 
-  # Tmp get rid of this
-  swapDevices = lib.mkIf (config.networking.hostName == "nixbox") [
-    {
-      device = "/var/swap";
-      size = 1024 * 32 * 2; # twice the RAM should leave enough space for hibernation
-    }
-  ];
-  #boot.resumeDevice = "/dev/mapper/cryptroot";
-
   # Writes to /etc/sysctl.d/60-nixos.conf
   boot.kernel.sysctl = {
     "vm.swappiness" = 0;
