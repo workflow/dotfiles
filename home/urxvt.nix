6753605df96e4b63c8946d83d9150e6b55e5a675
workflow <florian.peter@gmx.at>
2020-11-18 16:47:49 +0800

    Fancypants urxvt stuffs
    Thinking of switching to Alacritty #soon.

--- home/urxvt.nix
+++ home/urxvt.nix
@@ -6,14 +6,17 @@
 
     extraConfig = {
       # Perl extensions
-      perl-ext-common = "default,matcher,resize-font,vtwheel";
+      perl-ext-common = "default,matcher,resize-font,vtwheel,keyboard-select,-searchable-scrollback";
 
       # Matcher (clickable URLs)
       url-launcher = "${pkgs.xdg_utils}/bin/xdg-open";
       "matcher.button" = 1;
 
       # Messes with CTRL+SHIFT Keybindings, see https://wiki.archlinux.org/index.php/Rxvt-unicode#Perl_extensions
-      iso14755_52 = "false";
+      iso14755_52 = false;
+
+      # https://github.com/muennich/urxvt-perls#keyboard-select
+      "keyboard-select.clipboard" = true;
     };
 
     fonts = [ "xft:Fira Code Nerd Font:size=8" "xft:Inconsolata:size=8" ];
@@ -24,6 +27,10 @@
     keybindings = {
       "Shift-Control-C" = "eval:selection_to_clipboard";
       "Shift-Control-V" = "eval:paste_clipboard";
+
+      # https://github.com/muennich/urxvt-perls#keyboard-select
+      "Meta-Escape" = "perl:keyboard-select:activate";
+      "Meta-Shift-S" = "perl:keyboard-select:search";
     };
 
     package = pkgs.rxvt_unicode-with-plugins;
