b929a188da985867d991852a53ac28f44bcfca5c
workflow <florian.peter@gmx.at>
2021-06-17 12:53:49 +0800

    Forward USB devices for virt-manager Spice

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -4,6 +4,8 @@
   virtualisation.docker.enable = true;
 
   virtualisation.libvirtd.enable = true;
+  # Forward USB devices from unprivileged user for use by Spice USB Forwarding (via virt-manager)
+  security.wrappers.spice-client-glib-usb-acl-helper.source = "${pkgs.spice-gtk}/bin/spice-client-glib-usb-acl-helper";
 
   # TODO: This is insecure
   # See https://nixos.wiki/wiki/Docker
