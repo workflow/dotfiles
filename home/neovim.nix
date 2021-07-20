ce688e1e5621af56650af7108c550239640105a2
workflow <4farlion@gmail.com>
2021-07-20 16:13:07 +0800

    Change coc-references to ge
    Since gr conflicts with the gr plugin

--- home/neovim.nix
+++ home/neovim.nix
@@ -80,7 +80,7 @@ in
       nmap <silent> gd <Plug>(coc-definition)
       nmap <silent> gy <Plug>(coc-type-definition)
       nmap <silent> gi <Plug>(coc-implementation)
-      nmap <silent> gr <Plug>(coc-references)
+      nmap <silent> ge <Plug>(coc-references)
       " Use K to show documentation in preview window.
       nnoremap <silent> K :call <SID>show_documentation()<CR>
       function! s:show_documentation()
