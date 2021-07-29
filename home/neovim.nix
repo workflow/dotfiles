2f546dd66348a6beac31bdf0033594b0ea504524
workflow <4farlion@gmail.com>
2021-07-29 11:32:42 +0800

    Add vim mapping for :Rg

--- home/neovim.nix
+++ home/neovim.nix
@@ -168,6 +168,7 @@ in
       " FZF Settings
       nnoremap <C-p> :GFiles<cr>
       nnoremap <C-l> :Files<cr>
+      nnoremap <C-g> :Rg<cr>
 
       " Open up a simple file tree
       nnoremap <leader>pv :wincmd v<bar> :Ex <bar> :vertical resize 30<CR>
