dfc21efaaf9335bdcee53388282042bf41628807
workflow <4farlion@gmail.com>
2021-08-03 13:12:57 +0800

    Vim: change localleader and coc quicklist mappings

--- home/neovim.nix
+++ home/neovim.nix
@@ -27,7 +27,7 @@ in
       set shell=/run/current-system/sw/bin/bash
 
       let mapleader = ' '
-      let maplocalleader = ' '
+      let maplocalleader = ','
 
       " COC Settings
       nnoremap <silent> <leader>h :call CocActionAsync('doHover')<cr>
@@ -150,21 +150,21 @@ in
       set statusline^=%{coc#status()}%{get(b:,'coc_current_function',''\'''\')}
       " Mappings for CoCList
       " Show all diagnostics.
-      nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
+      nnoremap <silent><nowait> <localleader>a  :<C-u>CocList diagnostics<cr>
       " Manage extensions.
-      nnoremap <silent><nowait> <space>e  :<C-u>CocList extensions<cr>
+      nnoremap <silent><nowait> <localleader>e  :<C-u>CocList extensions<cr>
       " Show commands.
-      nnoremap <silent><nowait> <space>c  :<C-u>CocList commands<cr>
+      nnoremap <silent><nowait> <localleader>c  :<C-u>CocList commands<cr>
       " Find symbol of current document.
-      nnoremap <silent><nowait> <space>o  :<C-u>CocList outline<cr>
+      nnoremap <silent><nowait> <localleader>o  :<C-u>CocList outline<cr>
       " Search workspace symbols.
-      nnoremap <silent><nowait> <space>s  :<C-u>CocList -I symbols<cr>
+      nnoremap <silent><nowait> <localleader>s  :<C-u>CocList -I symbols<cr>
       " Do default action for next item.
-      nnoremap <silent><nowait> <space>j  :<C-u>CocNext<CR>
+      nnoremap <silent><nowait> <localleader>j  :<C-u>CocNext<CR>
       " Do default action for previous item.
-      nnoremap <silent><nowait> <space>k  :<C-u>CocPrev<CR>
+      nnoremap <silent><nowait> <localleader>k  :<C-u>CocPrev<CR>
       " Resume latest coc list.
-      nnoremap <silent><nowait> <space>p  :<C-u>CocListResume<CR>
+      nnoremap <silent><nowait> <localleader>p  :<C-u>CocListResume<CR>
 
       " FZF Settings
       nnoremap <C-p> :GFiles<cr>
