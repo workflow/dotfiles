d39a97fd814a14b38a6361c4f2e9bbe1f2a686d5
workflow <4farlion@gmail.com>
2021-08-11 13:01:47 +0800

    Add flexbox sound blocks

--- home/i3status-rust.nix
+++ home/i3status-rust.nix
@@ -27,6 +27,19 @@ let
     }
   ];
 
+  flexboxSoundBlocks = [
+    {
+      block = "sound";
+      format = "{output_name} {volume}";
+      on_click = "pavucontrol --tab=3";
+      mappings = {
+        "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
+        "alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo" = "";
+        "bluez_sink.14_3F_A6_28_DC_51.a2dp_sink" = "";
+      };
+    }
+  ];
+
   boarGPUBlocks = [
     {
       block = "nvidia_gpu";
@@ -141,6 +154,7 @@ in
         ]
         ++ lib.lists.optionals isBoar boarSoundBlocks
         ++ lib.lists.optionals isTopbox topboxSoundBlocks
+        ++ lib.lists.optionals isFlexbox flexboxSoundBlocks
         ++ [
           {
             block = "music";
