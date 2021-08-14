61c7ab6c9fe209c91e72ff69f20120c034c92d35
workflow <4farlion@gmail.com>
2021-08-15 04:15:10 +0800

    Add syncthing to boar-server

--- assets/root-configuration-boar-server.nix
+++ assets/root-configuration-boar-server.nix
@@ -50,6 +50,13 @@
     pkgs.neovim
   ];
 
+  # Syncthing
+  services.syncthing = {
+    enable = true;
+    user = "syncthing";
+    openDefaultPorts = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
