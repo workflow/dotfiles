812e7091a58fafdffcd0b7e6d648b10991cd00b1
workflow <4farlion@gmail.com>
2021-09-22 10:43:18 +0200

    Test out offload mode

--- assets/root-configuration-flexbox.nix
+++ assets/root-configuration-flexbox.nix
@@ -4,6 +4,15 @@
 
 { config, pkgs, ... }:
 
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in
 {
   imports =
     [
@@ -39,9 +48,10 @@
   # '';
 
   # GPU
+  environment.systemPackages = [ nvidia-offload ];
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.prime = {
-    sync.enable = true;
+    offload.enable = true;
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
