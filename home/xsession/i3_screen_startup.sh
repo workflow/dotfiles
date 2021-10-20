071f5f90b826f9a4bb890e35b2ac5c477407aefd
workflow <4farlion@gmail.com>
2021-10-21 00:28:40 +0100

    Update screen startup order

--- home/xsession/i3_screen_startup.sh
+++ home/xsession/i3_screen_startup.sh
@@ -1,8 +1,8 @@
 #! /run/current-system/sw/bin/bash
 set -euo pipefail
 
-autorandr flexbox || true
 autorandr flexbox-intel || true
+autorandr flexbox || true
 autorandr sophia || true
 autorandr boar || true
 autorandr topbox || true
