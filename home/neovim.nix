ea10b2e79e62e6ca7df05ece39892dd9d837d91d
workflow <4farlion@gmail.com>
2021-11-02 12:00:17 +0000

    Fix h mapping to ; (redo last f/F/t/T)

--- home/neovim.nix
+++ home/neovim.nix
@@ -14,11 +14,11 @@ in
       set number
       set clipboard=unnamedplus
 
+      noremap h ;
       noremap ; l
       noremap l k
       noremap k j
       noremap j h
-      noremap h ;
       noremap <C-w>; <C-w>l
       noremap <C-w>l <C-w>k
       noremap <C-w>k <C-w>j
