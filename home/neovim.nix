00ec0b48681f5a49c4edfd1dd34335fbbca1042e
workflow <4farlion@gmail.com>
2022-01-30 00:59:39 +0800

    Add vim fn to apply macro to visual selection

--- home/neovim.nix
+++ home/neovim.nix
@@ -201,6 +201,13 @@ in
       nnoremap <silent><nowait> <C-j> :cprev<cr>
       nnoremap <silent><nowait> <C-;> :cnext<cr>
 
+      " Applying a macro to lines matching in visual selection
+      " https://medium.com/@schtoeffel/you-don-t-need-more-than-one-cursor-in-vim-2c44117d51db
+      xnoremap @ :<C-u>call ExecuteMacroOverVisualRange()<CR>
+      function! ExecuteMacroOverVisualRange()
+        echo "@".getcmdline()
+        execute ":'<,'>normal @".nr2char(getchar())
+      endfunction
     '';
 
     plugins = with pkgs.vimPlugins; [
