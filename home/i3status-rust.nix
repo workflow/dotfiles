1ccc57fc5bcb9afb004a08096cf4e98dcb9ae65a
workflow <4farlion@gmail.com>
2022-03-24 09:37:58 +0000

    Refactor sound block mappings

--- home/i3status-rust.nix
+++ home/i3status-rust.nix
@@ -1,48 +1,26 @@
 # Themes and many setting looted from: https://github.com/Kthulu120/i3wm-themes/blob/master/Nature/.config/dunst/dunstrc
 { lib, config, pkgs, ... }:
 let
-  boarSoundBlocks = [
-    {
-      block = "sound";
-      format = "{output_name} {volume}";
-      on_click = "pavucontrol --tab=3";
-      mappings = {
-        "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
-        "alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo" = "";
-        "bluez_sink.14_3F_A6_28_DC_51.a2dp_sink" = "";
-      };
-    }
-  ];
-
-  topboxSoundBlocks = [
-    {
-      block = "sound";
-      format = "{output_name} {volume}";
-      on_click = "pavucontrol --tab=3";
-      mappings = {
-        "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
-        "alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo" = "";
-        "bluez_sink.14_3F_A6_28_DC_51.a2dp_sink" = "";
-      };
-    }
-  ];
-
-  flexboxSoundBlocks = [
-    {
-      block = "sound";
-      format = "{output_name} {volume}";
-      on_click = "pavucontrol --tab=3";
-      mappings = {
-        "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi___ucm0003.hw_sofsoundwire_2__sink" = "";
-        "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi___ucm0005.hw_sofsoundwire_2__sink" = "";
-        "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi___ucm0007.hw_sofsoundwire_2__sink" = "";
-        "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi__hw_sofsoundwire__sink" = "";
-        "alsa_output.usb-Apple__Inc._USB-C_to_3.5mm_Headphone_Jack_Adapter_DWH84440324JKLTA7-00.analog-stereo" = "";
-        "bluez_sink.14_3F_A6_28_DC_51.a2dp_sink" = "";
-      };
-      headphones_indicator = true;
-    }
-  ];
+  soundBlockMappings = {
+    "boar" = {
+      "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
+      "alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo" = "";
+      "bluez_sink.14_3F_A6_28_DC_51.a2dp_sink" = "";
+    };
+    "topbox" = {
+      "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
+      "alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo" = "";
+      "bluez_sink.14_3F_A6_28_DC_51.a2dp_sink" = "";
+    };
+    "flexbox" = {
+      "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi___ucm0003.hw_sofsoundwire_2__sink" = "";
+      "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi___ucm0005.hw_sofsoundwire_2__sink" = "";
+      "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi___ucm0007.hw_sofsoundwire_2__sink" = "";
+      "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi__hw_sofsoundwire__sink" = "";
+      "alsa_output.usb-Apple__Inc._USB-C_to_3.5mm_Headphone_Jack_Adapter_DWH84440324JKLTA7-00.analog-stereo" = "";
+      "bluez_sink.14_3F_A6_28_DC_51.a2dp_sink" = "";
+    };
+  };
 
   boarGPUBlocks = [
     {
@@ -155,10 +133,15 @@ in
             block = "backlight";
           }
         ]
-        ++ lib.lists.optionals isBoar boarSoundBlocks
-        ++ lib.lists.optionals isTopbox topboxSoundBlocks
-        ++ lib.lists.optionals isFlexbox flexboxSoundBlocks
         ++ [
+          {
+            block = "sound";
+            driver = "pulseaudio";
+            format = "{output_description} {volume}";
+            on_click = "pavucontrol --tab=3";
+            mappings = lib.attrsets.attrByPath [ "${sysconfig.networking.hostName}" ] { } soundBlockMappings;
+            headphones_indicator = true;
+          }
           {
             block = "music";
             player = "spotify";
