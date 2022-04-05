4add4aa1f8f0462ef88358c5c507b1b80829487e
workflow <4farlion@gmail.com>
2022-04-05 09:01:44 +0100

    Switch github protocol due to nixpkgs js errs

--- home/git.nix
+++ home/git.nix
@@ -17,6 +17,12 @@
         init = {
           defaultBranch = "main";
         };
+        # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/javascript.section.md#git-protocol-error
+        url = {
+          "https://github.com" = {
+            insteadOf = "git://github.com";
+          };
+        };
       };
 
       ignores = [ ".idea" "nohup.out" "mzdata" ];
