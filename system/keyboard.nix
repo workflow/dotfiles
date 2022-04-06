ed00494f55f5c4fefde8dab031c47b677abe754f
workflow <4farlion@gmail.com>
2022-04-06 11:19:29 +0100

    Add pt keyboard layout

--- system/keyboard.nix
+++ system/keyboard.nix
@@ -3,7 +3,7 @@
 {
   # writes to /etc/X11/xorg.conf.d
   services.xserver = {
-    layout = "us,de,ua";
+    layout = "us,de,ua,pt";
     # FIXME: Stopped working in 21.11, now setting manually via i3 startup
     #xkbOptions = "grp:ctrls_toggle,eurosign:e,caps:escape_shifted_capslock,terminate:ctrl_alt_bksp";
   };
