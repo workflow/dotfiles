a249534ece28f2a30f795d7e022c8a482b10de2e
workflow <4farlion@gmail.com>
2022-04-18 13:18:47 +0100

    use gruvbox-dark-soft rofi theme

--- home/rofi.nix
+++ home/rofi.nix
@@ -1,11 +1,4 @@
 { config, lib, pkgs, ... }:
-let
-  # Colors from: https://github.com/Kthulu120/i3wm-themes/blob/master/Nature/.resources/.extend.Xresources
-  color1 = "#162025";
-  color2 = "#662b37";
-  color3 = "#bfbfbf";
-
-in
 {
   programs.rofi = {
     enable = true;
@@ -15,37 +8,7 @@ in
       columns = 2;
     };
 
-    # theme = {
-    #   colors = {
-    #     rows = rec {
-    #       normal = {
-    #         background = color1;
-    #         foreground = color3;
-    #         backgroundAlt = color1;
-    #         highlight = {
-    #           background = color1;
-    #           foreground = color2;
-    #         };
-    #       };
-    #       active = normal;
-    #       urgent = normal;
-    #     };
-
-    #     window = {
-    #       background = color1;
-    #       border = color2;
-    #       separator = color2;
-    #     };
-    #   };
-
-    #   separator = "solid";
-
-    #   scrollbar = false;
-
-    #   padding = 5;
-
-    #   lines = 5;
-    # };
+    theme = "gruvbox-dark-soft";
 
     font = "Fira Code 12";
 
