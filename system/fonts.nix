725c216ce02c282da1320071f0e66256181ad39d
workflow <4farlion@gmail.com>
2022-05-10 21:14:58 +0100

    add font-awesome-5

--- system/fonts.nix
+++ system/fonts.nix
@@ -9,6 +9,7 @@ in
     pkgs.fira-code-symbols
     pkgs.font-awesome
     pkgs.font-awesome_4
+    pkgs.font-awesome_5
     pkgs.font-awesome-ttf
     (nixpkgs-unstable.nerdfonts.override { fonts = [ "FiraCode" "Inconsolata" ]; })
     pkgs.noto-fonts # For microsoft websites like Github and Linkedin on Chromium browsers
