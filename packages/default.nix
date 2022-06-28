0602dd890ad603003bd41dc6d1013d1532a559cc
workflow <4farlion@gmail.com>
2022-06-28 09:06:39 +0100

    feat(rust-analyzer): enable ra-multiplex
    See https://github.com/pr2502/ra-multiplex

--- packages/default.nix
+++ packages/default.nix
@@ -125,6 +125,7 @@ let
       rmview # Remarkable Screen Sharing
       pkgs.rnix-lsp
       pkgs.roboto
+      pkgs.rust-analyzer # for https://github.com/pr2502/ra-multiplex
       pkgs.screenkey
       pkgs.shellcheck
       nixpkgs-unstable.signal-desktop
