748732796a1eb1f597115114da9ac68d8c65bcc3
workflow <4farlion@gmail.com>
2022-07-25 12:07:11 +0100

    feat(prompt): tweak the starship

--- home/starship.nix
+++ home/starship.nix
@@ -6,9 +6,27 @@
     enableFishIntegration = true;
 
     settings = {
+      gcloud = {
+        format = "on [$symbol$account(@$domain)|($project)](green) ";
+      };
+
+      kubernetes = {
+        disabled = false;
+        style = "green";
+        context_aliases = {
+          "gke_northbound-staging_southamerica-east1-c_cluster-1" = "staging";
+          "gke_northbound-prod_southamerica-east1-c_cluster-1" = "prod";
+        };
+      };
+
       status = {
         disabled = false;
         symbol = "🔴 ";
+      };
+
+      nix_shell = {
+        format = "via [$symbol$state]($style) ";
+        impure_msg = "";
       };
     };
 
