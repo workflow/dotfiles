3bf58955ce2dc5afb4066a0f1e82e9679b730c5f
workflow <4farlion@gmail.com>
2022-09-08 09:56:26 +0100

    feat(fugitive): add :Gclog mapping

--- home/neovim.nix
+++ home/neovim.nix
@@ -297,6 +297,7 @@ in
       nnoremap <leader>gm :GMove<CR>
       nnoremap <leader>gb :Git checkout -b<Space>
       nnoremap <leader>gc :Git checkout<Space>
+      nnoremap <leader>gl :Gclog<CR>
       nnoremap <leader>gps :Git! push<CR>
       nnoremap <leader>gpf :Git! push --force-with-lease<CR>
       nnoremap <leader>gpl :Git! pull<CR>
