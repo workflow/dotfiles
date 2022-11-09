cad12e11dcb3ea69dc1068ff94446031ee156147
workflow <4farlion@gmail.com>
2022-11-09 13:53:07 +0000

    feat(screens): add flexbox-caparica profile

--- home/xsession/i3_screen_startup.sh
+++ home/xsession/i3_screen_startup.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 autorandr flexbox || true
 autorandr flexbox-intel-lo-res || true
+autorandr flexbox-caparica || true
 autorandr sophia || true
 autorandr boar || true
 autorandr caparica || true
