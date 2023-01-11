9ee35d3c7488a60a1ab5b456fae7b5bca6f85993
workflow <4farlion@gmail.com>
2023-01-11 19:40:18 +0000

    fix(wireguard): remove firewall overrides

--- system/networking.nix
+++ system/networking.nix
@@ -1,20 +1,9 @@
 { lib, config, pkgs, ... }:
 
 {
-  # Adapting the rpfilter to ignore Wireguard related traffic
-  # See https://nixos.wiki/wiki/WireGuard
   networking.firewall = {
-    # if packets are still dropped, they will show up in dmesg
+    # if packets are dropped, they will show up in dmesg
     logReversePathDrops = true;
-    # wireguard trips rpfilter up
-    extraCommands = ''
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-    '';
-    extraStopCommands = ''
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-    '';
   };
 
   networking.extraHosts = ''
