770b522ba7d85172d59ae45540b78525c6061b75
workflow <4farlion@gmail.com>
2023-01-12 20:36:16 +0000

    feat(xdg): make brave default for all links

--- home.nix
+++ home.nix
@@ -231,6 +231,12 @@ in
       enable = true;
       defaultApplications = {
         "text/html" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/http" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/https" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/about" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/unknown" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/webcal" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/mailto" = [ "brave-browser.desktop" ];
         "application/pdf" = [ "okular.desktop" ];
       };
     };
