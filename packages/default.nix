c66d07128bab07c5676022ca7fd6731fbab306df
workflow <4farlion@gmail.com>
2023-12-14 11:51:06 +0000

    feat(pkgs): add cht-sh

--- packages/default.nix
+++ packages/default.nix
@@ -31,6 +31,7 @@ let
       pkgs.cargo-edit
       pkgs.cargo-nextest
       pkgs.chafa # Images to terminal pixels
+      pkgs.cht-sh
       nixpkgs-unstable.llvmPackages.clang # For mold
       pkgs.cntr # for Nix sandbox breakpointHook debugging
       nixpkgs-unstable.comma
