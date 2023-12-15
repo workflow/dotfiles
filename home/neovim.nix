8ddbec48f1929a6032e97d6b1f8bfb37058a02a6
workflow <4farlion@gmail.com>
2023-12-15 19:32:33 +0000

    feat(nvim): enable which-key

--- home/neovim.nix
+++ home/neovim.nix
@@ -529,7 +529,15 @@ in
       {
         plugin = which-key-nvim;
         config = ''
+          vim.o.timeout = true
+          vim.o.timeoutlen = 300
+          require("which-key").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+          }
         '';
+        type = "lua";
       }
     ];
 
