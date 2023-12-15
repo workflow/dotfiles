11d6e823a9b46ffe642d1f18ef716d78a4d3686d
workflow <4farlion@gmail.com>
2023-12-15 19:54:54 +0000

    refactor(nvim): move ChatGPT-nvim keybings to whichkey

--- home/neovim.nix
+++ home/neovim.nix
@@ -408,14 +408,11 @@ in
       " Background light/dark toggling
       nmap <silent> <leader>i  :let &bg=(&bg=='light'?'dark':'light')<CR>
 
-      " ChatGPT
-      nmap <silent> <leader>cc :ChatGPT<CR>
-      nmap <silent> <leader>cp :ChatGPTActAs<CR>
-      nmap <silent> <leader>ce :ChatGPTEditWithInstructions<CR>
-      xmap <silent> <leader>ce :<C-U>ChatGPTEditWithInstructions<CR>
-
       " Overseer
       lua require('overseer').setup()
+    '';
+
+    extraLuaConfig = ''
     '';
 
     plugins = with pkgs.vimPlugins; [
@@ -460,6 +457,25 @@ in
         plugin = nixpkgs-unstable.vimPlugins.ChatGPT-nvim;
         config = ''
           require("chatgpt").setup({ })
+          local wk = require("which-key")
+          wk.register({
+            c = {
+              name = "ChatGPT",
+                c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+                e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+                g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+                t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+                k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+                d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+                a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+                o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+                s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+                f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+                x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+                r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+                l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+              },
+          }, { prefix = "<leader>" })
         '';
         type = "lua";
       }
