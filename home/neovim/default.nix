cc9e8fca52648c1687c4daa84e4fdb6002415a2b
workflow <4farlion@gmail.com>
2023-12-17 19:58:43 +0000

    fix(nvim): fix startify bookmark to main Nvim config

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -616,7 +616,7 @@ in
           let g:startify_change_to_dir = 0
           let g:startify_change_to_vcs_root = 1
           let g:startify_session_persistence = 1
-          let g:startify_bookmarks = [ {'c': '~/nixos-config/home/neovim.nix'} ]
+          let g:startify_bookmarks = [ {'c': '~/nixos-config/home/neovim/default.nix'} ]
         '';
       }
       vim-surround
