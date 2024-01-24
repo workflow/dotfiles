4e7a0372dc4211b12f3449597ce919a0e8b37269
workflow <4farlion@gmail.com>
2024-01-24 11:41:05 +0000

    feat(nvim): add yamlls with some sane defaults

--- home/neovim/lsp.lua
+++ home/neovim/lsp.lua
@@ -132,6 +132,25 @@ local servers = {
       command = 'clippy',
     },
   },
+  yamlls = {
+    schemas = {
+      kubernetes = "*.yaml",
+      ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+      ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+      ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+      ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+      ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+      ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+      ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+      ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+      ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+      "*docker-compose*.{yml,yaml}",
+      ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
+      "*flow*.{yml,yaml}",
+    },
+  },
 }
 
 -- Setup neovim lua configuration
