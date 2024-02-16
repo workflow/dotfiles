3dcd892019de60c32d53a6e1a0df6ca9e5301c75
workflow <4farlion@gmail.com>
2024-02-16 23:48:17 +0000

    feat(virtualisation): add distrobox running via podman

--- packages/default.nix
+++ packages/default.nix
@@ -35,6 +35,7 @@ let
       pkgs.delta # Syntax highlighter for git
       inputs.devenv.packages.x86_64-linux.devenv
       nixpkgs-unstable.discord
+      pkgs.distrobox
       pkgs.docker-compose
       pkgs.duplicati
       pkgs.dunst
--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -5,6 +5,7 @@
     enable = true;
     setSocketVariable = true;
   };
+
   # https://rootlesscontaine.rs/getting-started/common/cgroup2/#enabling-cpu-cpuset-and-io-delegation
   # For minikube
   # Writes to /etc/systemd/system/user@.service.d/overrides.conf
@@ -13,6 +14,10 @@
   };
 
   virtualisation.libvirtd.enable = true;
+
+  virtualisation.podman = {
+    enable = true;
+  };
 
   users.users.farlion.extraGroups = [ "libvirtd" "kvm" ];
 
