4273ad623a1001022fb98e8cb8eb6cca663d9ad8
workflow <4farlion@gmail.com>
2024-05-02 19:01:27 +0100

    refac(nvim): factor out telescope module

---
+++ home/neovim/telescope/default.nix
@@ -0,0 +1,66 @@
+{ pkgs, ... }:
+{
+
+  programs.neovim.plugins = with pkgs.unstable.vimPlugins; [
+    {
+      plugin = telescope-nvim;
+      config = ''
+        local builtin = require("telescope.builtin")
+        local utils = require("telescope.utils")
+        require("telescope").setup({
+          defaults = {
+            mappings = {
+              i = {
+                ["<C-s>"] = require("telescope.actions").select_horizontal,
+              },
+              n = {
+                ["<C-s>"] = require("telescope.actions").select_horizontal,
+              },
+            },
+          },
+        })
+        require("telescope").load_extension("fzf")
+        local wk = require("which-key")
+        wk.register({
+          ["<space>"] = {
+            name = "Find[ ](Telescope)",
+              ["<space>"] = { "<cmd>Telescope git_files<CR>", "Version Controlled Files" },
+              b = { "<cmd>Telescope buffers<CR>", "[B]uffers" },
+              f = { "<cmd>Telescope find_files<CR>", "All [F]iles" },
+              g = { "<cmd>Telescope live_grep<CR>", "[G]rep" },
+              m = { "<cmd>Telescope bookmarks list<CR>", "Book[m]arks" },
+              ["?"] = { "<cmd>Telescope keymaps<CR>", "Vim Keymap Cheatsheet" },
+              ["."] = { function() builtin.find_files({ cwd = utils.buffer_dir() }) end, "Files in CWD" },
+            },
+        }, { prefix = "<leader>" })
+      '';
+      type = "lua";
+    }
+    {
+      plugin = telescope-fzf-native-nvim;
+    }
+    {
+      plugin = telescope-frecency-nvim;
+      config = ''
+        require("telescope").setup({
+          extensions = {
+            frecency = {
+              show_scores = true,
+              auto_validate = false, -- manually gc with :FrecencyValidate
+            },
+          },
+        })
+        require("telescope").load_extension("frecency")
+        local wk = require("which-key")
+        wk.register({
+          ["<space>"] = {
+            name = "Find[ ](Telescope)",
+              h = { "<Cmd>Telescope frecency workspace=CWD<CR>", "History (Frecency)" },
+            },
+        }, { prefix = "<leader>" })
+      '';
+      type = "lua";
+    }
+  ];
+
+}
