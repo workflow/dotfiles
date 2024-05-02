9d94ad29948f4ed0d92480fe439501a09e01aaf4
workflow <4farlion@gmail.com>
2024-05-02 19:29:32 +0100

    feat(nvim): disable treesitter highlighting on large files (and Java for now)

--- home/neovim/treesitter/default.nix
+++ home/neovim/treesitter/default.nix
@@ -4,24 +4,7 @@
   programs.neovim.plugins = with pkgs.unstable.vimPlugins; [
     {
       plugin = nvim-treesitter.withAllGrammars;
-      config = ''
-        -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-        vim.defer_fn(function()
-          require('nvim-treesitter.configs').setup {
-            auto_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-              enable = true,
-              keymaps = {
-                init_selection = '<c-s>',
-                node_incremental = '<c-s>',
-                node_decremental = '<c-M-s>',
-              },
-            },
-          }
-        end, 0)
-      '';
+      config = builtins.readFile ./treesitter.lua;
       type = "lua";
     }
     {
---
+++ home/neovim/treesitter/treesitter.lua
@@ -0,0 +1,27 @@
+local function ts_disable_long_files(_, bufnr)
+  return api.nvim_buf_line_count(bufnr) > 5000
+end
+
+local disabled_languages = { "java" }
+
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+  require('nvim-treesitter.configs').setup {
+    auto_install = false,
+    highlight = {
+      enable = true,
+      disable = function(lang, bufnr)
+        return vim.tbl_contains(disabled_languages, lang) or ts_disable_long_files(lang, bufnr)
+      end,
+    },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-s>',
+        node_incremental = '<c-s>',
+        node_decremental = '<c-M-s>',
+      },
+    },
+  }
+end, 0)
