efcb222d7076dfdac9520016c153862dbff6831d
workflow <4farlion@gmail.com>
2024-05-02 19:38:16 +0100

    feat(nvim): open/close dap UI automagically

--- home/neovim/dap/dap-ui.lua
+++ home/neovim/dap/dap-ui.lua
@@ -1,8 +1,23 @@
 local wk = require("which-key")
-require("dapui").setup({
+local dap, dapui = require("dap"), require("dapui")
+dapui.setup({
   wk.register({
     d = {
       t = { require('dapui').toggle, "[T]oggle UI" },
     },
   }, { prefix = "<leader>" })
 })
+
+-- Events to auto-open/close the UI
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
