9f533b2225c9e85cd607bd80312279b595e01f8c
workflow <4farlion@gmail.com>
2024-05-03 12:03:54 +0100

    feat(nvim): find hidden files with Telescope find_files

--- home/neovim/telescope/default.nix
+++ home/neovim/telescope/default.nix
@@ -18,6 +18,11 @@
               },
             },
           },
+          pickers = {
+            find_files = {
+              hidden = true,
+            },
+          },
         })
         require("telescope").load_extension("fzf")
         local wk = require("which-key")
