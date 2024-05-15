2daff42742b1568c6e9db6d3e82431bc0f891330
workflow <4farlion@gmail.com>
2024-05-15 22:24:31 +0200

    fix(nvim): wrap mapping

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -189,8 +189,8 @@ in
       vim.o.breakindent = true
 
       -- Remap for dealing with word wrap
-      vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-      vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+      vim.keymap.set('n', 'l', "v:count == 0 ? 'gk' : 'l'", { expr = true, silent = true })
+      vim.keymap.set('n', 'k', "v:count == 0 ? 'gj' : 'k'", { expr = true, silent = true })
 
       -- Save undo history
       -- TODO: sync this if useful
