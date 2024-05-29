14f3d77fb642e433374c762667c18e216821cf1f
workflow <4farlion@gmail.com>
2024-05-29 17:06:29 +0100

    fix(video): also load the kernel module for v4l2loopback

--- packages/default.nix
+++ packages/default.nix
@@ -152,7 +152,6 @@ let
       pkgs.unzip
       pkgs.usbutils # Provides lsusb
       pkgs.variety
-      pkgs.v4l-utils # Video4Linux2 -> configuring webcam
       pkgs.virt-manager
       pkgs.vlc
       pkgs.vnstat # Network Traffic Monitor
--- system/video/default.nix
+++ system/video/default.nix
@@ -1,6 +1,12 @@
-{config, ...}:
+{config, pkgs, ...}:
 {
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+  boot.kernelModules = [ "v4l2loopback" ];
+
+  environment.systemPackages = [
+      pkgs.v4l-utils # Video4Linux2 -> configuring webcam
+  ];
+
 }
 
 
