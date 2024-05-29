e790a2ee7ee61f12435963176e62ab6d77122b16
workflow <4farlion@gmail.com>
2024-05-29 17:09:57 +0100

    fix(video): allow Chrome and WebRTC apps to see the virtual OBS Cam

--- system/video/default.nix
+++ system/video/default.nix
@@ -1,5 +1,10 @@
 {config, pkgs, ...}:
 {
+  # Loopback video device :)
+  # exclusive_caps=1 is needed for Chrome/WebRTC apps to be able to see the virtual cam
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=7 card_label="OBS Cam" exclusive_caps=1
+  '';
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" ];
 
