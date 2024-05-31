1091ddd5656ffc4ce1e6e973a9a4076ec6ee2de2
workflow <4farlion@gmail.com>
2024-05-31 17:17:55 +0100

    feat(nvim): use nerdfonts with dadbod-ui

--- home/neovim/dadbod/default.nix
+++ home/neovim/dadbod/default.nix
@@ -3,6 +3,10 @@
   programs.neovim.plugins = with pkgs.unstable.vimPlugins; [
     {
       plugin = vim-dadbod; # SQL Fu
+      config = ''
+        vim.g.db_ui_use_nerd_fonts = 1
+      '';
+      type = "lua";
     }
     {
       plugin = vim-dadbod-completion;
