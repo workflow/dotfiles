14491bd0f03c6a935a5e62d131309db2ab4c520e
workflow <4farlion@gmail.com>
2024-05-31 16:32:40 +0100

    feat(video): add some awesome OBS plugins

--- home/obs/default.nix
+++ home/obs/default.nix
@@ -3,6 +3,8 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
       obs-vintage-filter
     ];
   };
