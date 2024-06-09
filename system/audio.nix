fc8ab49df90eb099edbec1f42d77aeb5616a98c0
workflow <4farlion@gmail.com>
2024-06-09 22:30:32 +0100

    feat(wireplumber): enable fancy bluez codecs

--- system/audio.nix
+++ system/audio.nix
@@ -1,48 +1,5 @@
 { pkgs, ... }:
 {
-  # Disable unused outputs
-  # TODO: Not working, migrate to new format on Nixos Upgrade
-  environment.etc."wireplumber/wireplumber.conf.d/51-alsa-disable.conf".text = ''
-    monitor.alsa.rules = [
-      {
-        matches = [
-          {
-            device.name = "alsa_card.pci-0000_01_00.1"
-          }
-        ]
-        actions = {
-          update-props = {
-             device.disabled = true
-          }
-        }
-      },
-      {
-        matches = [
-          {
-            node.name = "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi__hw_sofsoundwire_5__sink.2"
-          }
-        ]
-        actions = {
-          update-props = {
-             node.disabled = true
-          }
-        }
-      },
-      {
-        matches = [
-          {
-            node.name = "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi__hw_sofsoundwire_6__sink"
-          }
-        ]
-        actions = {
-          update-props = {
-             node.disabled = true
-          }
-        }
-      }
-    ]
-  '';
-
   environment.systemPackages = with pkgs; [
       alsa-utils
       pulseaudioFull
@@ -56,6 +13,15 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.extraConfig = {
+      # Enable Fancy Blueooth Codecs
+      "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+    };
   };
 
   # Enable sound.enable to persist the ALSA store, see https://github.com/NixOS/nixpkgs/issues/130882#issuecomment-2137339830
