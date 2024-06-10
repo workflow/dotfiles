9086a766f6074c3854fdff6bd75efa9c45a6b63b
workflow <4farlion@gmail.com>
2024-06-10 16:34:45 +0100

    feat(nvim): add global mappings for toggling :DBUI

--- home/neovim/dadbod/default.nix
+++ home/neovim/dadbod/default.nix
@@ -3,16 +3,27 @@
   programs.neovim.plugins = with pkgs.unstable.vimPlugins; [
     {
       plugin = vim-dadbod; # SQL Fu
-      config = ''
-        vim.g.db_ui_use_nerd_fonts = 1
-      '';
-      type = "lua";
     }
     {
       plugin = vim-dadbod-completion;
     }
     {
       plugin = vim-dadbod-ui;
+      config = ''
+        vim.g.db_ui_use_nerd_fonts = 1
+
+        local wk = require("which-key")
+        wk.register({
+          ["<leader>"] = {
+            D = {
+              name = "[D]atabase",
+              t = { "<cmd>DBUIToggle<CR>", "[t]oggle" },
+              n = { "<cmd>tab DBUI<CR>", "open in [n]ew tab" },
+            },
+          },
+        })
+      '';
+      type = "lua";
     }
   ];
 }
