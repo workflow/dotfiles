0d8103327cbfd36830431b77ced847f9179926a0
workflow <4farlion@gmail.com>
2024-06-11 11:08:14 +0100

    feat(wireplumber): disable unused sinks and card

--- system/audio.nix
+++ system/audio.nix
@@ -13,6 +13,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
     wireplumber.extraConfig = {
       # Enable Fancy Blueooth Codecs
       "monitor.bluez.properties" = {
@@ -20,6 +21,60 @@
           "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
           "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+
+      # Disable unused sinks and sources
+      "disable-unused-nodes" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              {
+                "node.nick" = "HDMI / DisplayPort 1 Output";
+              }
+            ];
+            actions = {
+              update-props = {
+                "node.disabled" = true;
+              };
+            };
+          }
+          {
+            matches = [
+              {
+                "node.nick" = "HDMI / DisplayPort 2 Output";
+              }
+            ];
+            actions = {
+              update-props = {
+                "node.disabled" = true;
+              };
+            };
+          }
+          {
+            matches = [
+              {
+                "node.nick" = "HDMI / DisplayPort 3 Output";
+              }
+            ];
+            actions = {
+              update-props = {
+                "node.disabled" = true;
+              };
+            };
+          }
+          {
+            matches = [
+              {
+                "device.nick" = "HDA NVidia";
+              }
+            ];
+            actions = {
+              update-props = {
+                "device.disabled" = true;
+              };
+            };
+          }
+        ];
       };
     };
   };
