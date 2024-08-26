b09f7341ea6cff997a76f3171c2253a96a8df37c
workflow <4farlion@gmail.com>
2024-08-26 10:38:38 +0100

    feat(nvim): add C-W shortcut for terminal mode

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -194,6 +194,7 @@ in {
 
       -- Terminal Mode shortcuts
       vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { silent = true })
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], { silent = true })
     '';
 
     extraPackages = with pkgs.unstable; [
