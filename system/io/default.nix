eb40c36a8d7a6c5c0fa2b2f75ecfb343ec7753c3
workflow <4farlion@gmail.com>
2024-08-31 19:29:11 +0200

    feat(io): adjust touchpad accel

--- system/io/default.nix
+++ system/io/default.nix
@@ -14,6 +14,10 @@ in {
   # Writes to /etc/X11/xorg.conf.d
   services.libinput = {
     enable = true;
-    touchpad.disableWhileTyping = true;
+    touchpad = {
+      disableWhileTyping = true;
+      accelProfile = "adaptive";
+      accelSpeed = "0.2";
+    };
   };
 }
