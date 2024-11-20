92d94d8de29db0b4da239b1607a544d9e7e74b66
workflow <4farlion@gmail.com>
2024-11-20 20:54:39 +0000

    feat(sound-levels-maintainer): maintain blue mic input gain

--- home/sound-levels-maintainer/scripts/sound-levels-maintainer-boar.sh
+++ home/sound-levels-maintainer/scripts/sound-levels-maintainer-boar.sh
@@ -10,16 +10,20 @@ get_pipewire_node_id() {
 
 OBS_SPEAKER_ID=$(get_pipewire_node_id "ObsSpeaker")
 OBS_MIC_ID=$(get_pipewire_node_id "ObsMic")
+BLUE_MIC_ID=$(get_pipewire_node_id "alsa_input.usb-Generic_Blue_Microphones_LT_221104181411AD020101_111000-00.analog-stereo")
 
 wpctl set-volume "$OBS_SPEAKER_ID" 1.0
 wpctl set-volume "$OBS_MIC_ID" 1.0
+wpctl set-volume "$BLUE_MIC_ID" 1.0
 
 while true; do
 	current_volume_obs_speaker=$(wpctl get-volume "$OBS_SPEAKER_ID" | awk '{print $2}')
 	current_volume_obs_mic=$(wpctl get-volume "$OBS_MIC_ID" | awk '{print $2}')
+	current_volume_blue_mic=$(wpctl get-volume "$BLUE_MIC_ID" | awk '{print $2}')
 
 	[ "$current_volume_obs_speaker" != "1.0" ] && wpctl set-volume "$OBS_SPEAKER_ID" 1.0
 	[ "$current_volume_obs_mic" != "1.0" ] && wpctl set-volume "$OBS_MIC_ID" 1.0
+	[ "$current_volume_blue_mic" != "1.0" ] && wpctl set-volume "$BLUE_MIC_ID" 1.0
 
 	sleep 1
 done
