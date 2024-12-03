b81509d3f4a0c5daf7f8bc1c34593bae23556a92
workflow <4farlion@gmail.com>
2024-12-03 15:43:24 -0800

    toil(redshift): temporarily adjust geolocation

--- home/redshift.nix
+++ home/redshift.nix
@@ -3,8 +3,10 @@
     enable = true;
 
     # Until Mozilla is fixed, fetch from https://www.geonames.org/
-    latitude = 38.74724;
-    longitude = -9.24526;
+    # latitude = 38.74724;
+    # longitude = -9.24526;
+    latitude = 37.61882;
+    longitude = -122.3758;
 
     provider = "manual";
     tray = true;
