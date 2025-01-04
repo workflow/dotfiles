ddb630bf6255e5cea9c5fd50f2fcb00f4d17624a
workflow <4farlion@gmail.com>
2025-01-04 00:39:32 +0000

    feat(nvidia): switch to open drivers

--- system/nvidia/default.nix
+++ system/nvidia/default.nix
@@ -8,7 +8,7 @@
     pkgs.libva-utils
   ];
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
   };
 
@@ -33,7 +33,7 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
