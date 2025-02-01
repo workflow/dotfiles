b036eff35a54c5fc3b684b38b3759f6a77271634
workflow <4farlion@gmail.com>
2025-02-01 00:01:33 +0000

    feat(avante): tweak settings

--- home/neovim/avante/avante.lua
+++ home/neovim/avante/avante.lua
@@ -1,3 +1,15 @@
 require('avante_lib').load()
 require('avante').setup({
+	dual_boost = {
+		enabled = true,
+	},
+	hints = { enabled = false }
 })
+local wk = require("which-key")
+wk.add(
+	{
+		{
+			{ "<leader>a", group = "[A]vante" },
+		},
+	}
+)
