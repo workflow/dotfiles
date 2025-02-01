f9259ab34258d10492ba9724cf065f7930fb72db
workflow <4farlion@gmail.com>
2025-02-01 00:27:57 +0000

    feat(avante): add azure endpoint for dual boost

--- home/neovim/avante/avante.lua
+++ home/neovim/avante/avante.lua
@@ -1,7 +1,13 @@
 require('avante_lib').load()
 require('avante').setup({
+	azure = {
+		endpoint = "https://hl-openai-sweden.openai.azure.com",
+		deployment = "gpt-4o",
+		api_version = "2024-10-21",
+	},
 	dual_boost = {
 		enabled = true,
+		first_provider = "azure",
 	},
 	hints = { enabled = false }
 })
