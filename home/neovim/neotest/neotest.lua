ee82dcfb3e8cbc60a272815f3b51a9337f283a11
workflow <4farlion@gmail.com>
2025-02-17 18:00:52 +0000

    feat(neotest): update shortcuts

--- home/neovim/neotest/neotest.lua
+++ home/neovim/neotest/neotest.lua
@@ -12,11 +12,14 @@ local wk = require("which-key")
 wk.add(
   {
     { "<leader>n",  group = "[N]eotest" },
-    { "<leader>na", require("neotest").run.attach,                                   desc = "[A]ttach to nearest" },
-    { "<leader>nd", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "[D]ebug nearest" },
-    { "<leader>nf", function() require("neotest").run.run(vim.fn.expand("%")) end,   desc = "[F]ile" },
-    { "<leader>nn", require("neotest").run.run,                                      desc = "[N]earest" },
-    { "<leader>no", require("neotest").output_panel.toggle,                          desc = "Toggle [O]utput Panel" },
-    { "<leader>ns", require("neotest").summary.toggle,                               desc = "Toggle [S]ummary" },
+    { "<leader>na", require("neotest").run.attach,                                               desc = "[A]ttach to nearest" },
+    { "<leader>nd", function() require("neotest").run.run({ strategy = "dap" }) end,             desc = "[D]ebug nearest" },
+    { "<leader>nf", function() require("neotest").run.run(vim.fn.expand("%")) end,               desc = "[F]ile" },
+    { "<leader>nn", require("neotest").run.run,                                                  desc = "[N]earest" },
+    { "<leader>no", function()
+      require("neotest").output_panel.toggle()
+      vim.cmd("wincmd p")
+    end,                                                                                         desc = "Toggle [O]utput Panel" },
+    { "<leader>ns", function() require("neotest").summary.toggle({ enter = true }) end,          desc = "Toggle [S]ummary" },
   }
 )
