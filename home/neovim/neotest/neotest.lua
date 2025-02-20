4bf5e3fe585eee4e40195ceaf072e74e1c440146
workflow <4farlion@gmail.com>
2025-02-20 14:29:58 +0000

    feat(neotest): run in toggleterm

--- home/neovim/neotest/neotest.lua
+++ home/neovim/neotest/neotest.lua
@@ -6,20 +6,27 @@ require("neotest").setup({
       -- ignore_file_types = { "java" },
     }),
   },
+  consumers = {
+    overseer = require("neotest.consumers.overseer"),
+  },
 })
 
 local wk = require("which-key")
 wk.add(
   {
     { "<leader>n",  group = "[N]eotest" },
-    { "<leader>na", require("neotest").run.attach,                                               desc = "[A]ttach to nearest" },
-    { "<leader>nd", function() require("neotest").run.run({ strategy = "dap" }) end,             desc = "[D]ebug nearest" },
-    { "<leader>nf", function() require("neotest").run.run(vim.fn.expand("%")) end,               desc = "[F]ile" },
-    { "<leader>nn", require("neotest").run.run,                                                  desc = "[N]earest" },
-    { "<leader>no", function()
-      require("neotest").output_panel.toggle()
-      vim.cmd("wincmd p")
-    end,                                                                                         desc = "Toggle [O]utput Panel" },
-    { "<leader>ns", function() require("neotest").summary.toggle({ enter = true }) end,          desc = "Toggle [S]ummary" },
+    { "<leader>na", require("neotest").run.attach,                                   desc = "[A]ttach to nearest" },
+    { "<leader>nd", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "[D]ebug nearest" },
+    { "<leader>nf", function() require("neotest").run.run(vim.fn.expand("%")) end,   desc = "[F]ile" },
+    { "<leader>nn", require("neotest").run.run,                                      desc = "[N]earest" },
+    {
+      "<leader>no",
+      function()
+        require("neotest").output_panel.toggle()
+        vim.cmd("wincmd p")
+      end,
+      desc = "Toggle [O]utput Panel"
+    },
+    { "<leader>ns", function() require("neotest").summary.toggle({ enter = true }) end, desc = "Toggle [S]ummary" },
   }
 )
