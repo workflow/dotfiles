6e6c22b8526a4024712239947f5e27acab9c3a03
workflow <4farlion@gmail.com>
2025-02-20 16:46:58 +0000

    feat(dap): add default config for C, C++, Rust

--- home/neovim/dap/dap.lua
+++ home/neovim/dap/dap.lua
@@ -17,3 +17,18 @@ dap.adapters.codelldb = {
   type = "executable",
   command = "codelldb",
 };
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
