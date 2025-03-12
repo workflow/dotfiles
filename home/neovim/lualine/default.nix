868bd5b7df76a8b65445a9ed8c4052e4a17f7854
workflow <4farlion@gmail.com>
2025-03-12 08:43:08 +0100

    fix(nvim): properly set new lualine sections

--- home/neovim/lualine/default.nix
+++ home/neovim/lualine/default.nix
@@ -20,8 +20,31 @@
             lualine_a = {'mode'},
             lualine_b = {'branch', 'diff', 'diagnostics'},
             lualine_c = {'filename'},
-            lualine_w = {'overseer'},
-            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_x = {
+              {
+                require("noice").api.status.message.get_hl,
+                cond = require("noice").api.status.message.has,
+              },
+              {
+                require("noice").api.status.command.get,
+                cond = require("noice").api.status.command.has,
+                color = { fg = "#ff9e64" },
+              },
+              {
+                require("noice").api.status.mode.get,
+                cond = require("noice").api.status.mode.has,
+                color = { fg = "#ff9e64" },
+              },
+              {
+                require("noice").api.status.search.get,
+                cond = require("noice").api.status.search.has,
+                color = { fg = "#ff9e64" },
+              },
+              'overseer',
+              'encoding',
+              'fileformat',
+              'filetype'
+            },
             lualine_y = {'progress'},
             lualine_z = {'location'}
           },
