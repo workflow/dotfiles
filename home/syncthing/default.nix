f6ae8e54fd261d6f8af5053fedf40ecc74b92969
workflow <4farlion@gmail.com>
2025-03-14 22:48:58 +0100

    refac(sway): simplify syncthingtray fix

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{...}: {
   services.syncthing = {
     enable = true;
     tray = {
@@ -6,18 +6,8 @@
       command = "syncthingtray --wait";
     };
   };
-  # :Hack: to wait for swaybar (the tray) to be available
+
   systemd.user.services.syncthingtray = {
-    Install = lib.mkForce {}; # don't start automatically, trigger by timer below instead
-  };
-  systemd.user.timers.syncthingtray = {
-    Unit = {
-      Description = "Timer for Delaying the Start of Syncthing Tray";
-    };
-    Timer = {
-      Unit = "syncthingtray.service";
-      OnBootSec = "10s";
-    };
     Install.WantedBy = ["sway-session.target"];
   };
 }
