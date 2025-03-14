03ac1dbc2f7c7d5d534033f11a6b2428c1d8c5a0
workflow <4farlion@gmail.com>
2025-03-14 20:11:11 +0100

    fix(dunst): config file typo

--- home/services/dunst/default.nix
+++ home/services/dunst/default.nix
@@ -10,7 +10,7 @@
 
       ignore_flameshot_warning = {
         appname = "flameshot";
-        message = "flameshot: Flameshot Warning \ngrim's screenshot component is implemented based on wlroots, it may not be used in GNOME or similar desktop environments";
+        body = "ngrim's screenshot component is implemented based on wlroots, it may not be used in GNOME or similar desktop environments";
         skip_display = true;
       };
     };
