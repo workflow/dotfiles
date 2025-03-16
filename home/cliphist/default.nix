98ef0f5699a5108a012893375f92e390b8a4e154
workflow <4farlion@gmail.com>
2025-03-16 13:17:04 +0100

    fix(cliphist): add xdg-utils explicitly

--- home/cliphist/default.nix
+++ home/cliphist/default.nix
@@ -1,6 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   services.cliphist = {
     enable = true;
     systemdTarget = "sway-session.target";
   };
+
+  home.packages = [pkgs.xdg-utils]; # For image copy/pasting
 }
