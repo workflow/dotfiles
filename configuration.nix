12e100fe0391158aa64226ce257ae73802b0911f
workflow <4farlion@gmail.com>
2025-04-16 20:33:09 +0100

    refac(system): simplify module structure

--- configuration.nix
+++ configuration.nix
@@ -1,41 +1,15 @@
 {
   inputs,
-  isImpermanent,
-  lib,
   pkgs,
-  secrets,
   ...
 }: let
   packages = pkgs.callPackage ./packages {inherit inputs;};
 in {
-  imports =
-    [
-      ./nix
-
-      ./system/audio.nix
-      ./system/desktop.nix
-      ./system/dns.nix
-      ./system/firmware
-      ./system/fonts.nix
-      ./system/io
-      ./system/kind-killer
-      ./system/ledgerlive.nix
-      ./system/networking
-      ./system/nix-ld.nix
-      ./system/performance.nix
-      ./system/power.nix
-      ./system/printing.nix
-      ./system/security.nix
-      ./system/steam.nix
-      ./system/stylix
-      ./system/various
-      ./system/video
-      ./system/virtualisation.nix
-
-      ./specialisations/light
-    ]
-    ++ lib.lists.optionals isImpermanent [./system/impermanence]
-    ++ lib.lists.optionals (secrets ? systemSecrets) secrets.systemSecrets;
+  imports = [
+    ./nix
+    ./specialisations/light
+    ./system
+  ];
 
   environment.systemPackages = packages;
 }
---
+++ system/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  isImpermanent,
+  secrets,
+  ...
+}: {
+  imports =
+    [
+      ./audio.nix
+      ./desktop.nix
+      ./dns.nix
+      ./firmware
+      ./fonts.nix
+      ./io
+      ./kind-killer
+      ./ledgerlive.nix
+      ./networking
+      ./nix-ld.nix
+      ./performance.nix
+      ./power.nix
+      ./printing.nix
+      ./security.nix
+      ./steam.nix
+      ./stylix
+      ./various
+      ./video
+      ./virtualisation.nix
+    ]
+    ++ lib.lists.optionals isImpermanent [./impermanence]
+    ++ lib.lists.optionals (secrets ? systemSecrets) secrets.systemSecrets;
+}
