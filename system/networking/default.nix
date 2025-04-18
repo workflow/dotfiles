b9a97e91c5bc6b1b8ce4219a7c6988ccf985056b
workflow <4farlion@gmail.com>
2025-04-18 19:23:58 +0100

    toil: update secrets

--- system/networking/default.nix
+++ system/networking/default.nix
@@ -53,22 +53,4 @@
   networking.dhcpcd.enable = false;
   # Only wait for a single interface to come up
   systemd.network.wait-online.anyInterface = true;
-
-  # MacGyver
-  systemd.services.macgyver = {
-    description = "MacGyver";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${(pkgs.python3.withPackages (ps: with ps; [json5]))}/bin/python /home/farlion/code/dlh/common_scripts/setup_tools/sshforwarding/setup_forwarding.py --config_path /home/farlion/code/dlh/common_scripts/setup_tools/sshforwarding/";
-      Environment = "PATH=/run/current-system/sw/bin:$PATH";
-      Restart = "always";
-      RestartSec = 30;
-      User = "root";
-      Group = "root";
-      KillSignal = "SIGINT";
-    };
-  };
-
-  programs.wireshark.enable = true;
-  users.users.farlion.extraGroups = ["wireshark"];
 }
