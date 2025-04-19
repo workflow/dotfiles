768befe1010e678d07443d09fede2abbeefc35b6
workflow <4farlion@gmail.com>
2025-04-19 11:37:55 +0100

    feat(security): persist boltd state under impermanence

--- system/various/default.nix
+++ system/various/default.nix
@@ -32,8 +32,6 @@
   services.tzupdate.enable = true; # Oneshot systemd service, run with `sudo systemctl start tzupdate`
   i18n.defaultLocale = "en_US.UTF-8";
 
-  services.hardware.bolt.enable = true;
-
   # limit the amount of logs stored in /var/log/journal
   # writes to /etc/systemd/journald.conf
   services.journald.extraConfig = ''
