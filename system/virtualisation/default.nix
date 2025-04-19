87f321b2534f7b4dca1330078f6ebb0fc7c02791
workflow <4farlion@gmail.com>
2025-04-19 23:58:20 +0100

    feat(minikube): clean up leftovers

--- system/virtualisation/default.nix
+++ system/virtualisation/default.nix
@@ -47,13 +47,6 @@ in {
     allowedUDPPorts = [53];
   };
 
-  # https://rootlesscontaine.rs/getting-started/common/cgroup2/#enabling-cpu-cpuset-and-io-delegation
-  # For minikube
-  # Writes to /etc/systemd/system/user@.service.d/overrides.conf
-  # systemd.services."user@".serviceConfig = {
-  #   Delegate = "cpu cpuset io memory pids";
-  # };
-
   virtualisation.libvirtd.enable = true;
 
   virtualisation.podman = {
