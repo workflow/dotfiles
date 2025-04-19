6e586a9615509a27c0ab11430c99fd6506d81fe0
workflow <4farlion@gmail.com>
2025-04-19 10:56:56 +0100

    feat(io): clean up X11 libinput settings

--- system/io/default.nix
+++ system/io/default.nix
@@ -1,11 +1,2 @@
 {...}: {
-  # Writes to /etc/X11/xorg.conf.d
-  services.libinput = {
-    enable = true;
-    touchpad = {
-      disableWhileTyping = true;
-      accelProfile = "adaptive";
-      accelSpeed = "0.2";
-    };
-  };
 }
