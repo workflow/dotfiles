b9261b13e9e2ceb2cd79646461c3aa51154511cc
workflow <4farlion@gmail.com>
2025-04-19 23:13:28 +0100

    feat(easyeffects): add impermanence

--- home/devenv/default.nix
+++ home/devenv/default.nix
@@ -4,13 +4,13 @@
   pkgs,
   ...
 }: {
-  home.packages = [
-    pkgs.devenv
-  ];
-
   home.persistence."/persist/home/farlion/" = lib.mkIf isImpermanent {
     directories = [
       ".local/share/devenv"
     ];
   };
+
+  home.packages = [
+    pkgs.devenv
+  ];
 }
