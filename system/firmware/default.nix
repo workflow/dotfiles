27bdea6689929b3cd5eb2fa71bd1552244a90db2
workflow <4farlion@gmail.com>
2025-04-19 10:46:28 +0100

    feat(fwupd): add impermanence

--- system/firmware/default.nix
+++ system/firmware/default.nix
@@ -1,4 +1,14 @@
 # Linux Firmware Updates
-{...}: {
+{
+  lib,
+  isImpermanent,
+  ...
+}: {
+  environment.persistence."/persist" = lib.mkIf isImpermanent {
+    directories = [
+      "/var/lib/fwupd"
+    ];
+  };
+
   services.fwupd.enable = true;
 }
