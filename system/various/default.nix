146ded7599dd7da3ce7819e12e4212b5444364a2
workflow <4farlion@gmail.com>
2025-04-19 11:52:26 +0100

    feat(systemd): no longer limit log storage beyond defaults

--- system/various/default.nix
+++ system/various/default.nix
@@ -8,12 +8,6 @@
   services.tzupdate.enable = true; # Oneshot systemd service, run with `sudo systemctl start tzupdate`
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # limit the amount of logs stored in /var/log/journal
-  # writes to /etc/systemd/journald.conf
-  services.journald.extraConfig = ''
-    SystemMaxUse=2G
-  '';
-
   # Enable system-wide Yubikey Support
   services.udev.packages = [pkgs.yubikey-personalization];
 }
