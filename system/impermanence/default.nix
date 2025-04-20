9ca53b1640718565264443bc6b606b3da1108402
workflow <4farlion@gmail.com>
2025-04-20 02:57:13 +0100

    feat(impermanence): add actual 🧨

--- system/impermanence/default.nix
+++ system/impermanence/default.nix
@@ -1,6 +1,35 @@
 # General impermanence setup
 # Note: specifics should live with their respective modules, where possible!
-{}: {
+{lib}: {
+  # Explode / on every boot, see https://grahamc.com/blog/erase-your-darlings/
+  boot.initrd.postResumeCommands = lib.mkAfter ''
+    # Back up / with timestamp under /old_roots
+    mkdir /btrfs_tmp
+    mount /dev/nixos_vg/root /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
+
+    # 🧨
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    # Garbage Collect: old_roots older than 30 days
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
+
   boot.tmp.cleanOnBoot = true;
 
   fileSystems."/persist/".neededForBoot = true;
