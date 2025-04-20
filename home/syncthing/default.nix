f921cf9d9cccf7992b4c986112fa25bd648a5dd0
workflow <4farlion@gmail.com>
2025-04-20 02:27:37 +0100

    feat(syncthingtray): persist config file

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -8,6 +8,9 @@
       ".local/state/syncthing" # device keys and certificates
       ".config/syncthing" # pre-v1.27.0 uses this instead of $XDG_STATE_HOME above, keeping for backward-compatibility
     ];
+    files = [
+      ".config/syncthingtray.ini"
+    ];
   };
 
   services.syncthing = {
