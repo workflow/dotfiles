711052f443e3370ceb928e94f0301410c923c90b
workflow <4farlion@gmail.com>
2025-04-20 02:13:41 +0100

    feat(nvim/copilot): add impermanence

--- home/neovim/copilot/default.nix
+++ home/neovim/copilot/default.nix
@@ -1,4 +1,14 @@
-{pkgs, ...}: {
+{
+  isImpermanent,
+  lib,
+  pkgs,
+  ...
+}: {
+  home.persistence."/persist/home/farlion/" = lib.mkIf isImpermanent {
+    directories = [
+      ".config/github-copilot"
+    ];
+  };
   programs.neovim.plugins = with pkgs.vimPlugins; [
     {
       plugin = copilot-vim;
