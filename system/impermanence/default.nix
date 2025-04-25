ac290c211f4be1a5e17038e7103c9f294210c111
workflow <4farlion@gmail.com>
2025-04-25 14:03:20 +0100

    feat(impermanence): remove garbage collection of old_roots until it's more stable
    See https://github.com/nix-community/impermanence/issues/250

--- system/impermanence/default.nix
+++ system/impermanence/default.nix
@@ -18,25 +18,6 @@
   #      fi
   #  fi
 
-  #  # Recursively Garbage Collect: old_roots older than 30 days
-  #  delete_subvolume_recursively() {
-  #      IFS=$'\n'
-
-  #      # If we accidentally end up with a file or directory under old_roots,
-  #      # the code will enumerate all subvolumes under the main volume.
-  #      # We don't want to remove everything under true main volume. Only
-  #      # proceed if this path is a btrfs subvolume (inode=256).
-  #      if [ $(stat -c %i "$1") -ne 256 ]; then return; fi
-
-  #      for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #          delete_subvolume_recursively "/btrfs_tmp/$i"
-  #      done
-  #      btrfs subvolume delete "$1"
-  #  }
-  #  #for i in $(find /btrfs_tmp/persist/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30); do
-  #  #    delete_subvolume_recursively "$i"
-  #  #done
-
   #  btrfs subvolume create /btrfs_tmp/root
   #  umount /btrfs_tmp
   #'';
