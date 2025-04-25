d196ae704e6eaa5c3b641d73a5a3488921e3c778
workflow <4farlion@gmail.com>
2025-04-26 00:53:06 +0100

    refac(impermanence): rework scripts on a systemd initrd basis

--- system/impermanence/default.nix
+++ system/impermanence/default.nix
@@ -1,53 +1,84 @@
 # General impermanence setup
 # Note: specifics should live with their respective modules, where possible!
-{lib, ...}: {
+{pkgs, ...}: let
+  rootExplosion = ''
+    echo "Time to 🧨" >/dev/kmsg
+    # Back up / with timestamp under /old_roots
+    mkdir /btrfs_tmp
+    mount /dev/mapper/nixos--vg-root /btrfs_tmp
+
+    # TODO: uncomment for actual 🧨
+    # Root impermanence
+    # if [[ -e /btrfs_tmp/root ]]; then
+    #     mkdir -p /btrfs_tmp/persist/old_roots
+    #     timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%d_%H:%M:%S")
+    #     if [[ ! -e /btrfs_tmp/persist/old_roots/$timestamp ]]; then
+    #       mv /btrfs_tmp/root "/btrfs_tmp/persist/old_roots/$timestamp"
+    #     else
+    #       btrfs subvolume delete /btrfs_tmp/root
+    #     fi
+    # fi
+
+    ###
+    # GC
+    ###
+    # Recursively Garbage Collect: old_roots older than 30 days
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+
+        # If we accidentally end up with a file or directory under old_roots,
+        # the code will enumerate all subvolumes under the main volume.
+        # We don't want to remove everything under true main volume. Only
+        # proceed if this path is a btrfs subvolume (inode=256).
+        if [ $(stat -c %i "$1") -ne 256 ]; then return; fi
+
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            echo "Performing GC on $i" >/dev/kmsg
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+    latest_snapshot=$(find /btrfs_tmp/persist/old_roots/ -mindepth 1 -maxdepth 1 -type d | sort -r | head -n 1)
+    # Only delete old snapshots if there's at least one that will remain after deletion
+    if [ -n "$latest_snapshot" ]; then
+        for i in $(find /btrfs_tmp/persist/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30 | grep -v -e "$latest_snapshot"); do
+
+            delete_subvolume_recursively "$i"
+        done
+    fi
+
+    # TODO: uncomment for actual 🧨
+    # btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+    echo "Done with 🧨. Au revoir!" >/dev/kmsg
+  '';
+in {
   # Explode / on every boot and resume, see https://grahamc.com/blog/erase-your-darlings/
-  # boot.initrd.postResumeCommands = lib.mkAfter ''
-  #   # Back up / with timestamp under /old_roots
-  #   mkdir /btrfs_tmp
-  #   mount /dev/mapper/nixos--vg-root /btrfs_tmp
-  #
-  #   # Root impermanence
-  #   if [[ -e /btrfs_tmp/root ]]; then
-  #       mkdir -p /btrfs_tmp/persist/old_roots
-  #       timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%d_%H:%M:%S")
-  #       if [[ ! -e /btrfs_tmp/persist/old_roots/$timestamp ]]; then
-  #         mv /btrfs_tmp/root "/btrfs_tmp/persist/old_roots/$timestamp"
-  #       else
-  #         btrfs subvolume delete /btrfs_tmp/root
-  #       fi
-  #   fi
-  #
-  #   # Recursively Garbage Collect: old_roots older than 30 days
-  #   delete_subvolume_recursively() {
-  #       IFS=$'\n'
-  #
-  #       # If we accidentally end up with a file or directory under old_roots,
-  #       # the code will enumerate all subvolumes under the main volume.
-  #       # We don't want to remove everything under true main volume. Only
-  #       # proceed if this path is a btrfs subvolume (inode=256).
-  #       if [ $(stat -c %i "$1") -ne 256 ]; then return; fi
-  #
-  #       for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #           delete_subvolume_recursively "/btrfs_tmp/$i"
-  #       done
-  #       btrfs subvolume delete "$1"
-  #   }
-  #
-  #   ###
-  #   # GC
-  #   ###
-  #   latest_snapshot=$(find /btrfs_tmp/persist/old_roots/ -mindepth 1 -maxdepth 1 -type d | sort -r | head -n 1)
-  #   # Only delete old snapshots if there's at least one that will remain after deletion
-  #   if [ -n "$latest_snapshot" ]; then
-  #       for i in $(find /btrfs_tmp/persist/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30 | grep -v -e "$latest_snapshot"); do
-  #           delete_subvolume_recursively "$i"
-  #       done
-  #   fi
-  #
-  #   btrfs subvolume create /btrfs_tmp/root
-  #   umount /btrfs_tmp
-  # '';
+  boot.initrd.systemd = {
+    extraBin = {
+      grep = "${pkgs.gnugrep}/bin/grep";
+    };
+    services = {
+      root-explode = {
+        wantedBy = ["initrd-root-device.target"];
+        wants = ["lvm2-activation.service"];
+        # See https://github.com/nix-community/impermanence/issues/250#issuecomment-2603848867
+        after = ["lvm2-activation.service" "local-fs-pre.target"];
+        before = ["sysroot.mount"];
+        # Run on cold boot only, never on resume from hibernation
+        unitConfig = {
+          ConditionKernelCommandLine = ["!resume="];
+          RequiresMountsFor = ["/dev/mapper/nixos--vg-root"];
+        };
+        serviceConfig = {
+          StandardOutput = "journal+console";
+          StandardError = "journal+console";
+          Type = "oneshot";
+        };
+        script = rootExplosion;
+      };
+    };
+  };
 
   boot.tmp.cleanOnBoot = true;
 
