0096f38ba9193e2ef007738bcfb9f7af843abb3b
workflow <4farlion@gmail.com>
2025-06-04 14:04:49 +0100

    style(home/stylix): apply icon theme

--- home/stylix/default.nix
+++ home/stylix/default.nix
@@ -3,6 +3,12 @@
     pkgs.papirus-icon-theme
   ];
   stylix = {
+    iconTheme = {
+      enable = true;
+      package = pkgs.papirus-icon-theme;
+      light = "Papirus-Light";
+      dark = "Papirus-Dark";
+    };
     targets = {
       firefox = {
         profileNames = ["main"];
