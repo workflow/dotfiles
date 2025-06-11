3700201c8200b1a5449ff99da552b3455fee6981
workflow <4farlion@gmail.com>
2025-06-11 11:58:46 +0100

    feat(fish): clean up theming and other stuff

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -106,48 +106,10 @@
   ];
 
   shellInit = ''
-    if test -e $HOME/.local-fishrc
-      source $HOME/.local-fishrc
-    end
-
     thefuck --alias | source
 
     ${variables}
-    ${theme}
   '';
-
-  theme =
-    /*
-    fish
-    */
-    ''
-      set -g fish_color_autosuggestion 586e75
-      set -g fish_color_cancel -r
-      set -g fish_color_command 93a1a1
-      set -g fish_color_comment 586e75
-      set -g fish_color_cwd green
-      set -g fish_color_cwd_root red
-      set -g fish_color_end 268bd2
-      set -g fish_color_error dc322f
-      set -g fish_color_escape 00a6b2
-      set -g fish_color_history_current --bold
-      set -g fish_color_host normal
-      set -g fish_color_match --background=blue
-      set -g fish_color_normal normal
-      set -g fish_color_operator 00a6b2
-      set -g fish_color_param 839496
-      set -g fish_color_quote 657b83
-      set -g fish_color_redirection 6c71c4
-      set -g fish_color_search_match bryellow --background=405555
-      set -g fish_color_selection white --bold --background=black
-      set -g fish_color_status red
-      set -g fish_color_user brgreen
-      set -g fish_color_valid_path --underline
-      set -g fish_pager_color_completion B3A06D
-      set -g fish_pager_color_description B3A06D
-      set -g fish_pager_color_prefix cyan --underline
-      set -g fish_pager_color_progress brwhite --background=cyan
-    '';
 
   variables =
     /*
