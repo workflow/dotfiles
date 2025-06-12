037cca147af62cc72b4e33d59a15728c1ce998d9
workflow <4farlion@gmail.com>
2025-06-12 23:24:15 +0100

    feat(tzupdate): disable automatic timed TZ updates

--- system/various/default.nix
+++ system/various/default.nix
@@ -3,7 +3,10 @@
 
   boot.supportedFilesystems = ["ntfs"];
 
-  services.tzupdate.enable = true; # Oneshot systemd service, run with `sudo systemctl start tzupdate`
+  services.tzupdate = {
+    enable = true; # Oneshot systemd service, run with `sudo systemctl start tzupdate`
+    timer.enable = false; # Disable automatic TZ updates
+  };
 
   i18n.defaultLocale = "en_US.UTF-8";
 }
