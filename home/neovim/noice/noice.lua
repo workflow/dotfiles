cc3af7b884dafcbba35bf4653c1f8fcc58771a9a
workflow <4farlion@gmail.com>
2025-06-16 16:37:29 +0100

    feat(nvim/noice): remove broken cmdline tweaks

--- home/neovim/noice/noice.lua
+++ home/neovim/noice/noice.lua
@@ -9,29 +9,13 @@ require("noice").setup({
 	},
 	-- you can enable a preset for easier configuration
 	presets = {
-		bottom_search = true,   -- use a classic bottom cmdline for search
+		bottom_search = true, -- use a classic bottom cmdline for search
 		command_palette = true, -- position the cmdline and popupmenu together
 		long_message_to_split = true, -- long messages will be sent to a split
-		inc_rename = false,     -- enables an input dialog for inc-rename.nvim
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
 		lsp_doc_border = false, -- add a border to hover docs and signature help
 	},
 })
-
--- Cmdline auto-accept tweaks
-vim.keymap.set("c", "<CR>", function()
-	if require("noice").api.cmdline.is_active() then
-		-- Check if there's a completion visible
-		if vim.fn.pumvisible() == 1 then
-			return "<C-y>" -- Accept completion
-		else
-			return "<CR>" -- Normal enter
-		end
-	else
-		return "<CR>"
-	end
-end, { expr = true })
--- And map Ctrl+Enter for directly executing cmdline without completion
-vim.keymap.set("c", "<C-CR>", "<CR>", { expr = false })
 
 local wk = require("which-key")
 wk.add(
