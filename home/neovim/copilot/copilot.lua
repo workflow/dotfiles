ebed2ef627ad18bfdb2c4b605885c91b883caa66
workflow <4farlion@gmail.com>
2025-06-17 13:45:58 +0100

    feat(nvim/copilot-lua): replicate tpope settings

--- home/neovim/copilot/copilot.lua
+++ home/neovim/copilot/copilot.lua
@@ -1,1 +1,12 @@
-require("copilot").setup({})
+require("copilot").setup({
+	panel = {
+		enabled = false,
+	},
+	suggestion = {
+		auto_trigger = true,
+		hide_during_completion = false,
+		keymap = {
+			accept = '<Tab>',
+		},
+	},
+})
