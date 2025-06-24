80c87a75fdbb1ebee24d4dc5632c348e17373320
workflow <4farlion@gmail.com>
2025-06-24 20:56:53 +0100

    feat(waybar/clock): fancyize

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -254,6 +254,35 @@ in {
             deactivated = "";
           };
         };
+
+        clock = {
+          format = "{:%H:%M}";
+          format-alt = "{:%A, %B %d, %Y (%R)}  ";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "year";
+            mode-mon-col = 3;
+            weeks-pos = "right";
+            on-scroll = 1;
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          actions = {
+            on-click-right = "mode";
+            on-click-middle = "brave calendar.google.com";
+            on-scroll-up = "shift_up";
+            on-scroll-down = "shift_down";
+          };
+        };
+
+        tray = {
+          show-passive-items = true;
+        };
       };
     };
 
