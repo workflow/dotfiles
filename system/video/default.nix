eaf9ae71f59773bde382f97943cf7724389117de
workflow <4farlion@gmail.com>
2025-07-08 14:51:15 +0100

    refac(obs-studio/virtual-cam): make use of `enableVirtualCamera` option, remove old kernel workarounds

--- system/video/default.nix
+++ system/video/default.nix
@@ -1,28 +1,8 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  # Loopback video device :)
-  # exclusive_caps=1 is needed for Chrome/WebRTC apps to be able to see the virtual cam
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=7 card_label="OBS Cam" exclusive_caps=1
-  '';
-  # OBS virtual cam fix until OBS 31.1 is released
-  boot.extraModulePackages = [
-    (pkgs.linuxPackages_zen.v4l2loopback.overrideAttrs (_: {
-      version = "0.13.2-manual";
-      src = pkgs.fetchFromGitHub {
-        owner = "umlaeute";
-        repo = "v4l2loopback";
-        rev = "v0.13.2";
-        hash = "sha256-rcwgOXnhRPTmNKUppupfe/2qNUBDUqVb3TeDbrP5pnU=";
-      };
-    }))
-  ];
-  # TODO: Once fixed, consider using NixOS programs.obs-studio.enableVirtualCamera instead
-  # boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
-  boot.kernelModules = ["v4l2loopback"];
+{pkgs, ...}: {
+  programs.obs-studio = {
+    enable = true;
+    enableVirtualCamera = true;
+  };
 
   environment.systemPackages = [
     pkgs.v4l-utils # Video4Linux2 -> configuring webcam
