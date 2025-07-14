0f89f2840f02dd64085d6e32dcf3f6638df1eda1
workflow <4farlion@gmail.com>
2025-07-14 12:40:19 +0100

    feat(niri/spawns): add seahorse to unlock keyring

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -129,6 +129,7 @@ in {
       {command = ["systemctl" "--user" "restart" "xdg-desktop-portal-gtk"];} # Fix for portal startup delay, see https://github.com/sodiboo/niri-flake/issues/509
       {command = ["ytmdesktop"];}
       {command = ["todoist-electron" "--ozone-platform-hint=auto"];}
+      {command = ["seahorse"];} # To unlock keyring
       {command = ["systemctl" "--user" "restart" "kanshi"];}
       {command = ["wlsunset-waybar"];}
       {command = ["zen"];}
