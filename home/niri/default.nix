9bf5c78b786ceb09eb5e418fbf169c9da491b1d8
workflow <4farlion@gmail.com>
2025-07-21 23:39:07 +0100

    feat(niri/spawn-at-startup): add obsidian

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -150,6 +150,7 @@ in {
       {command = ["systemctl" "--user" "restart" "xdg-desktop-portal-gtk"];} # Fix for portal startup delay, see https://github.com/sodiboo/niri-flake/issues/509
       {command = ["systemctl" "--user" "restart" "kanshi"];}
       {command = ["systemctl" "--user" "restart" "app-blueman@autostart"];}
+      {command = ["obsidian"];}
       {command = ["ytmdesktop"];}
       {command = ["todoist-electron" "--ozone-platform-hint=auto"];}
       {command = ["seahorse"];} # To unlock keyring
