0c8d85874615564bf148d5a8de5579ac4c45e926
workflow <4farlion@gmail.com>
2025-07-29 16:32:50 +0100

    fix(comma): temporary version incompatibility

--- home/nix-index/default.nix
+++ home/nix-index/default.nix
@@ -2,6 +2,7 @@
   inputs,
   isImpermanent,
   lib,
+  pkgs,
   ...
 }: {
   home.persistence."/persist/home/farlion" = lib.mkIf isImpermanent {
@@ -17,5 +18,9 @@
     enable = true;
   };
 
-  programs.nix-index-database.comma.enable = true;
+  # Use unstable comma to fix compatibility with nix-index 0.1.9
+  programs.nix-index-database.comma.enable = false;
+  home.packages = [
+    pkgs.unstable.comma
+  ];
 }
