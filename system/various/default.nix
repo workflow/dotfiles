f4782800c6090e70650842308018418bf581f9f3
workflow <4farlion@gmail.com>
2025-09-24 00:19:08 +0300

    fix(various/tzupdate): startup timing issue

--- system/various/default.nix
+++ system/various/default.nix
@@ -7,6 +7,17 @@
     enable = true; # Oneshot systemd service, run with `sudo systemctl start tzupdate`
     timer.enable = false; # Disable automatic TZ updates
   };
+  # Fix tzupdate networking dependency issue
+  systemd.services.tzupdate = {
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
+    serviceConfig = {
+      # Add retry logic in case network is still not fully ready
+      Restart = "on-failure";
+      RestartSec = "30s";
+      RestartMode = "direct";
+    };
+  };
 
   i18n.defaultLocale = "en_US.UTF-8";
 }
