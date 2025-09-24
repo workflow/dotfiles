dff7c7607aa32ec3bad19eaf2c100fe515ef3c80
workflow <4farlion@gmail.com>
2025-09-24 22:21:22 +0300

    feat(nvim/cmp): switch to manual copilot mode

--- home/neovim/copilot/copilot.lua
+++ home/neovim/copilot/copilot.lua
@@ -1,12 +1,3 @@
 require("copilot").setup({
-	panel = {
-		enabled = false,
-	},
-	suggestion = {
-		auto_trigger = true,
-		hide_during_completion = false,
-		keymap = {
-			accept = '<C-y>',
-		},
-	},
+
 })
