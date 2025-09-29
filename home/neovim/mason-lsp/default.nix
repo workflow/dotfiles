8a90828afad4d3d9455a27963dde37d2bb6d89e0
workflow <4farlion@gmail.com>
2025-09-29 21:04:42 +0300

    feat(nvim/lsp): enable inline diagnostics via virtual text

--- home/neovim/mason-lsp/default.nix
+++ home/neovim/mason-lsp/default.nix
@@ -1,18 +1,38 @@
 {pkgs, ...}: {
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-    {
-      plugin = mason-nvim; # Automatically install LSP servers
-      config = builtins.readFile ./mason.lua;
-      runtime = {
-        "lua/shared_lsp_config.lua".source = ./shared_lsp_config.lua;
-      };
-      type = "lua";
-    }
-    {
-      plugin = mason-lspconfig-nvim; # Automatically install LSP servers
-    }
-    {
-      plugin = mason-nvim-dap-nvim; # Automatically configure DAP adapters
-    }
-  ];
+  programs.neovim = {
+    extraLuaConfig = ''
+      -- LSP diagnostics: show inline virtual text
+      vim.diagnostic.config({
+        virtual_text = {
+          spacing = 2,
+          prefix = "●",
+        },
+        signs = true,          -- keep signs in the gutter
+        underline = true,      -- underline offending code
+        update_in_insert = false, -- reduce noise while typing
+        severity_sort = true,  -- sort diagnostics by severity
+        float = {
+          border = "rounded",
+          source = "if_many",
+        },
+      })
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = mason-nvim; # Automatically install LSP servers
+        config = builtins.readFile ./mason.lua;
+        runtime = {
+          "lua/shared_lsp_config.lua".source = ./shared_lsp_config.lua;
+        };
+        type = "lua";
+      }
+      {
+        plugin = mason-lspconfig-nvim; # Automatically install LSP servers
+      }
+      {
+        plugin = mason-nvim-dap-nvim; # Automatically configure DAP adapters
+      }
+    ];
+  };
 }
