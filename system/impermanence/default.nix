2f9834996e693c4b2f33e9092ffee9cb9bc78626
workflow <4farlion@gmail.com>
2025-10-23 11:29:21 +0100

    refac(system/impermanence): make use of recursive BTRFS subvol delete
    See https://github.com/nix-community/impermanence/pull/271/files

--- system/impermanence/default.nix
+++ system/impermanence/default.nix
@@ -20,28 +20,11 @@
     ###
     # GC
     ###
-    # Recursively Garbage Collect: old_roots older than 30 days
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-
-        # If we accidentally end up with a file or directory under old_roots,
-        # the code will enumerate all subvolumes under the main volume.
-        # We don't want to remove everything under true main volume. Only
-        # proceed if this path is a btrfs subvolume (inode=256).
-        if [ $(stat -c %i "$1") -ne 256 ]; then return; fi
-
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            echo "Performing GC on $i" >/dev/kmsg
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
     latest_snapshot=$(find /btrfs_tmp/persist/old_roots/ -mindepth 1 -maxdepth 1 -type d | sort -r | head -n 1)
     # Only delete old snapshots if there's at least one that will remain after deletion
     if [ -n "$latest_snapshot" ]; then
         for i in $(find /btrfs_tmp/persist/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30 | grep -v -e "$latest_snapshot"); do
-
-            delete_subvolume_recursively "$i"
+            btrfs subvolume delete -R "$i"
         done
     fi
 
