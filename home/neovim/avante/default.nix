9f363a11d1085cca3d7b73fd2b0690d9a1829322
workflow <4farlion@gmail.com>
2025-10-28 16:39:04 +0000

    fix(impermanence/avante): persist current model selection

--- home/neovim/avante/default.nix
+++ home/neovim/avante/default.nix
@@ -1,5 +1,15 @@
 # Cursor style AI IDE
-{pkgs, ...}: {
+{
+  isImpermanent,
+  lib,
+  pkgs,
+  ...
+}: {
+  home.persistence."/persist" = lib.mkIf isImpermanent {
+    directories = [
+      ".config/avante.nvim" # Some Avante state like last used model
+    ];
+  };
   programs.neovim = {
     extraLuaConfig = ''
       -- views can only be fully collapsed with the global statusline
