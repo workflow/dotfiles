34ae0d172a49dc23a761fca1f02d74e413ab15f3
workflow <4farlion@gmail.com>
2025-10-28 17:56:31 +0000

    fix(nvim/avante): pin to 0.0.27-unstable-2025-10-06
    Next version introduces bugs like broken history, lots of hangs.

--- home/neovim/avante/default.nix
+++ home/neovim/avante/default.nix
@@ -4,7 +4,19 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  # Pin avante-nvim to 0.0.27-unstable-2025-10-06, with the next version introducing lots of bugs like broken history and hangs.
+  pinnedNixpkgs = pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "bce5fe2bb998488d8e7e7856315f90496723793c";
+    sha256 = "sha256-vwXL9H5zDHEQA0oFpww2one0/hkwnPAjc47LRph6d0I=";
+  };
+  pinnedPkgs = import pinnedNixpkgs {
+    system = pkgs.system;
+    config = pkgs.config;
+  };
+in {
   home.persistence."/persist" = lib.mkIf isImpermanent {
     directories = [
       ".config/avante.nvim" # Some Avante state like last used model
@@ -15,9 +27,9 @@
       -- views can only be fully collapsed with the global statusline
       vim.opt.laststatus = 3
     '';
-    plugins = with pkgs.unstable.vimPlugins; [
+    plugins = [
       {
-        plugin = avante-nvim;
+        plugin = pinnedPkgs.vimPlugins.avante-nvim;
         config = builtins.readFile ./avante.lua;
         type = "lua";
       }
