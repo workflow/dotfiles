4cd2fba274be7d0ba43eeae30b5da6acbe820c64
workflow <4farlion@gmail.com>
2025-11-03 20:15:25 +0000

    feat(jujutsu): add signing

--- home/jujutsu/default.nix
+++ home/jujutsu/default.nix
@@ -9,6 +9,11 @@
         email = "4farlion@gmail.com";
         name = "workflow";
       };
+      signing = {
+        backend = "gpg";
+        key = "24575DB93F6CEC16";
+        behavior = "own"; # sign commits you authored on modify
+      };
     };
   };
 }
