a27cdf3284ee2929914637b73f2a44121c073d5a
workflow <4farlion@gmail.com>
2025-12-03 00:08:22 +0000

    feat(home/bash): add <C-x> keybinding to copy command line

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   isImpermanent,
+  pkgs,
   ...
 }: {
   home.persistence."/persist" = lib.mkIf isImpermanent {
@@ -8,5 +9,16 @@
       ".bash_history"
     ];
   };
-  programs.bash.enable = true;
+
+  programs.bash = {
+    enable = true;
+    package = pkgs.bashInteractive;
+    initExtra = ''
+      # Ctrl-x: Copy current command line to clipboard
+      copy-command-line() {
+        printf '%s' "$READLINE_LINE" | ${pkgs.wl-clipboard}/bin/wl-copy
+      }
+      bind -x '"\C-x": copy-command-line'
+    '';
+  };
 }
