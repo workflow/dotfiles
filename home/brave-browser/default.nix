167762448310dcdebfcbf292c0158cc7297f9075
workflow <4farlion@gmail.com>
2026-01-20 13:45:50 +0000

    fix(brave-browser): set terminal=false in desktop entry

--- home/brave-browser/default.nix
+++ home/brave-browser/default.nix
@@ -45,7 +45,7 @@ in {
       icon = "brave-browser";
       mimeType = ["application/pdf" "application/rdf+xml" "application/rss+xml" "application/xhtml+xml" "application/xhtml_xml" "application/xml" "image/gif" "image/jpeg" "image/png" "image/webp" "text/html" "text/xml" "x-scheme-handler/http" "x-scheme-handler/https" "x-scheme-handler/ipfs" "x-scheme-handler/ipns"];
       startupNotify = true;
-      terminal = true;
+      terminal = false;
       type = "Application";
     };
   };
