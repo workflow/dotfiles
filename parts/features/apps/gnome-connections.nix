07757a89004a94ae50b829c7322d74d29b491ed9
workflow <4farlion@gmail.com>
2026-02-04 14:14:55 +0000

    fix(impermanence): remove duplicate persistence paths

--- parts/features/apps/gnome-connections.nix
+++ parts/features/apps/gnome-connections.nix
@@ -8,7 +8,6 @@
     home.persistence."/persist" = lib.mkIf osConfig.dendrix.isImpermanent {
       directories = [
         ".config/freerdp" # RDP server certificates/keys
-        ".config/dconf" # GNOME settings database (GSettings/dconf)
       ];
       files = [".config/connections.db"]; # GNOME Connections connection profiles database
     };
