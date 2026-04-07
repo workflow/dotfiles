6ab7eb45a0fa7cc76dcf973750cf1f3b5e5f0458
workflow <4farlion@gmail.com>
2026-04-07 10:47:40 +0100

    feat(video): add stable udev symlinks for webcams on numenor

--- parts/features/hardware/video.nix
+++ parts/features/hardware/video.nix
@@ -1,5 +1,12 @@
 {...}: {
-  flake.modules.nixos.video = {pkgs, ...}: {
+  flake.modules.nixos.video = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    isNumenor = config.dendrix.hostname == "numenor";
+  in {
     programs.obs-studio = {
       enable = true;
       enableVirtualCamera = true;
@@ -8,6 +15,12 @@
     environment.systemPackages = [
       pkgs.v4l-utils # Video4Linux2 -> configuring webcam
     ];
+
+    # Stable symlinks for webcams so OBS scenes always get the right camera
+    services.udev.extraRules = lib.mkIf isNumenor ''
+      SUBSYSTEM=="video4linux", ATTR{index}=="0", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0e03", SYMLINK+="video-razer-kiyo"
+      SUBSYSTEM=="video4linux", ATTR{index}=="0", ATTRS{idVendor}=="2e1a", ATTRS{idProduct}=="4c03", SYMLINK+="video-insta360-link"
+    '';
 
     users.users.farlion.extraGroups = ["video"];
   };
