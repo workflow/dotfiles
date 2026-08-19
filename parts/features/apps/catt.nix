a8290cc9fcbf9631424d6fdcb6944f7db46f53e8
workflow <4farlion@gmail.com>
2026-08-20 00:41:39 +0200

    toil(catt,go2tv): move firewall openings out of this repo

--- parts/features/apps/catt.nix
+++ parts/features/apps/catt.nix
@@ -1,12 +1,5 @@
 # Cast All The Things - send media to Chromecast devices
 {...}: {
-  flake.modules.nixos.catt = {
-    networking.firewall.allowedTCPPorts = [
-      45114 # catt local media server
-      45115 # catt subtitles server
-    ];
-  };
-
   flake.modules.homeManager.catt = {
     osConfig,
     lib,
--- parts/features/apps/go2tv.nix
+++ parts/features/apps/go2tv.nix
@@ -1,11 +1,5 @@
 # Cast media to UPnP/DLNA renderers (e.g. LG webOS TVs)
 {...}: {
-  flake.modules.nixos.go2tv = {
-    networking.firewall.allowedTCPPorts = [
-      3500 # go2tv media server + UPnP event callbacks
-    ];
-  };
-
   flake.modules.homeManager.go2tv = {
     osConfig,
     lib,
